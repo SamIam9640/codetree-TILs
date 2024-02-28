ad1f4fbdbc275fe8915ebbee08e996d9c2da84b8
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-02-28 20:12:36 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240228/배수의 개수 2/number-of-multipliers-2.cpp
+++ 240228/배수의 개수 2/number-of-multipliers-2.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+	int a,cnt=0;
 
-	
-	int cnt = 0;
+	for(int i=1;i<=10;i++){
+		cin>>a;
 
-	for (int i = 1; i < 11; i++) {
-        int n;
-        cin >> n;
-		if (i % 2 == 1) {
+		if(a%2!=0){
 			cnt++;
 		}
-     
 	}
-
-	cout << cnt;
+	cout<<cnt;
 	return 0;
-
 }
