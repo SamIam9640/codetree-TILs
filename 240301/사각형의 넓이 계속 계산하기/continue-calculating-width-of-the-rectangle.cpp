1736c0bf0af67cb42149f63f58efb1997e0ec0ac
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-01 00:51:08 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240301/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    char h;
+
+    while(true){
+        cin>>a>>b>>h;
+        cout<<a*b<<"\n";
+
+        if (h=='C')
+        {
+            break;
+        }
+    }
+    return 0;
+}
