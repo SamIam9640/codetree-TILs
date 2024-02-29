a1c12e28b913f120863d13816016074a8e467bde
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-02-29 13:17:31 +0900

    Title: 숫자 사이의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/숫자 사이의 배수/multiplication-between-numbers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,sum_val=0,cnt=0;
+    double avg_val;
+    cin>>a>>b;
+
+    for(int i=a;i<=b;i++){
+        if(i%5==0||i%7==0){
+            sum_val +=i;
+            cnt++;
+        }
+    }
+    
+
+    avg_val=(double)sum_val/cnt;
+
+    cout<<fixed;
+    cout.precision(1);
+    cout<<sum_val<<" "<<avg_val;
+    return 0;
+}
