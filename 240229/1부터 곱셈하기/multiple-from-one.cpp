e163d88cc73ba2b97ba5dc4a0a301d0da59cb747
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-02-29 23:44:00 +0900

    Title: 1부터 곱셈하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/1부터 곱셈하기/multiple-from-one.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    int prod=1;
+    cin>>n;
+
+    for(int i=1;i<=10;i++){
+        prod *=i;
+        if(prod>=n){
+            cout<<i;
+            break;
+        }
+    }
+    return 0;
+}
