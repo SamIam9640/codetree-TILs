fbaa1d10e7d5a21a5055a673cf71dd54fc875529
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-02-29 13:59:56 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240229/범위 안의 수 2/number-in-range-2.cpp
+++ 240229/범위 안의 수 2/number-in-range-2.cpp
@@ -1,29 +1,19 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    // 변수 선언 및 입력
-    int num;
-    int sum = 0, cnt = 0;
-    double avg;
-
-    // 입력을 받으며 0이상 200이하의 정수들을 선별합니다.
-    for(int i = 1; i <= 10; i++) {
-        cin >> num;
-        if(num >= 0 && num <= 200) {
-            sum += num;
-            cnt++;
-        }
+    int b,sum_val=0,cnt=0;
+double avg_val;
+    for(int i=1;i<=10;i++){
+        cin>>b;
+        if(b>=0&&b<=200){
+        sum_val +=b;
+        cnt++;}
     }
+    avg_val =(double)sum_val/cnt;
 
-    // 0이상 200이하의 정수들의 평균을 구합니다.
-    avg = (double)sum / cnt;
-
-    // 출력
-    cout << fixed;
+    cout<<fixed;
     cout.precision(1);
-    cout << sum << " " << avg;
-
+    cout<<sum_val<<" "<<avg_val;
     return 0;
 }
