2df00d9ea5d9395a330026819fb220d86404c363
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-02-29 11:28:09 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240229/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,cnt=0;
+    cin>>a;
+
+    for(int i=1;i<=a;i++){
+        if((i%400==0)|| i%4==0&&i%100!=0){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
