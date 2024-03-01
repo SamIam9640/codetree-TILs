0c7dddb692bcbc6a334ff917f6c7eb3f7451af2e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-01 17:07:13 +0900

    Title: 합성수 판별기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240301/합성수 판별기/factorization-discriminator.cpp
+++ 240301/합성수 판별기/factorization-discriminator.cpp
@@ -5,12 +5,13 @@ int n;
 cin>>n;
 bool satisfied= false;
 
-for(int i=2;i<n;i++){
+for(int i=1;i<=n;i++){
     if(n%i==0)
     satisfied=true;
+}
     if(satisfied==true)
     cout<<"C";
     else cout<<"N";
     return 0;
-}
+
 }
