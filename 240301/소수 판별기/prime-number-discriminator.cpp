c60013c23a74492620a88f02b8a131965d55442e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-01 21:24:13 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240301/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+
+    bool satisfied=true;
+    for(int i=2;i<=n;i++){
+        if(n%i==0){
+            satisfied=false;
+        }
+    }
+    if (satisfied==true){
+        cout<<"P";
+    }
+    else {
+        cout<<"C";
+        
+    }
+    return 0;
+}
