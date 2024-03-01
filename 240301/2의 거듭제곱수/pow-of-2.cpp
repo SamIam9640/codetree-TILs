948c04b7a75d7ae80bc238a543ae142f910e8fa4
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-01 12:58:47 +0900

    Title: 2의 거듭제곱수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240301/2의 거듭제곱수/pow-of-2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int cnt=0;
+
+    while(n%2==0){
+        n/=2;
+        cnt++;
+
+    }
+    cout<<cnt;
+    return 0;
+}
