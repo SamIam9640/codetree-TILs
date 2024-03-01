1e7b8b408f8bd57b9b7c6fc68d9376bd4233d3c5
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-01 23:48:10 +0900

    Title: 별표 출력하기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240301/별표 출력하기 5/print-star-5.cpp
+++ 240301/별표 출력하기 5/print-star-5.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     int n;
     cin>>n;
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<(n-i);j++){
-            for(int k=0;k<n-i;k++)
-            cout<<"*";
-            cout<<" ";
+        for(int j=0;j<n-i;j++){
+            for(int k=0;k<n-i;k++){
+                cout<<"*";}
+                cout<<" ";
+            
+            
         }
-        cout<<"\n";
-        
+    cout<<"\n";
     }
-return 0;
+    
+    return 0;
 }
