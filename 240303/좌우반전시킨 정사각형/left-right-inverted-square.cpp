04400538a312ba1835b4bc312e84aac774c8852a
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-03 08:48:38 +0900

    Title: 좌우반전시킨 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/좌우반전시킨 정사각형/left-right-inverted-square.cpp
+++ 240303/좌우반전시킨 정사각형/left-right-inverted-square.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j--){
+        for(int j=1;j<=n;j++){
             cout<<i*(n-j+1)<<" ";
         }
         cout<<"\n";
