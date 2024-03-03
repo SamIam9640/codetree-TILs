a3e9a1954256d0e74c440fae98f10175de3ebae0
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-03 14:26:11 +0900

    Title: 좌우 지그재그 출력, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/좌우 지그재그 출력/print-lr-zigzag.cpp
+++ 240303/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -9,14 +9,15 @@ int main() {
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(i%2==0){
-                cout<<(i*n)+j+1<<" ";
+                cout<<(i*n)+j+1<<" ";}
                 else
                 cout<<(i*n)+n-j<<" ";
             }
+            cout<<"\n";
         }
-       cout<<"\n";
+       return 0;
         }
-           return 0;
+           
     
 
 
