0464b87b6d0f6a0f77415be14574ce343330df8f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-05 17:20:48 +0900

    Title: 정상적인 구구단, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/정상적인 구구단/normal-multiple.cpp
+++ 240305/정상적인 구구단/normal-multiple.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+    int n;
+    cin>>n;
 
-    for(int i=1;i<4;i++){
-        for(int j=1;j<4;j++){
-            cout<<i<<" * "<<j<< " = "<<i*j;
-            if(j<3){
-                cout<<", ";
-            }   
+    for
+    (int i=1;i<=n;i++){
+        for( int j=1;j<=n;j++){
+            cout<<i<<" * "<<j<<" = "<<i*j;
+            if(j<n) cout<<", ";
         }
-          cout<<"\n";
+        cout<<"\n";
     }
     return 0;
 }
