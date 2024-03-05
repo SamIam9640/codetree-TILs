1f38ee02c4a485b130c4887f5d1a66d12ad1bc8e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-05 17:52:21 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    for(int i=2;i<9;i+=2){
+        for(int j=b;j>=a;j--){
+            cout<<j<<" * "<<i<<" = "<<j*i;
+            if(j!=a)cout<<" / ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
