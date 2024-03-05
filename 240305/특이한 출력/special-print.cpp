3c3c0bbf8579dce9b511552f7859cf0f802a2958
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-05 23:07:31 +0900

    Title: 특이한 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240305/특이한 출력/special-print.cpp
+++ 240305/특이한 출력/special-print.cpp
@@ -5,11 +5,12 @@ int main() {
     int n;
     cin>>n;
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=3;j++){
             if((i+j)%4==0) cout<<"("<<i<<", "<<j<<") "<<"\n";
             else cout<<"("<<i<<", "<<j<<") ";
-        }
     }
-    return 0;
+    
+}
+return 0;
 }
