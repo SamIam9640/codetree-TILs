4124d53a10f4b5d16986ef195f4149d03b9e22af
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-06 17:26:36 +0900

    Title: 3n + 1 수열 m번, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/3n + 1 수열 m번/3n-plus-1-sequence-m-times.cpp
+++ 240306/3n + 1 수열 m번/3n-plus-1-sequence-m-times.cpp
@@ -1,21 +1,33 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+
+int calculateStepsToReachOne(int n) {
+    int steps = 0;
+
+    while (n != 1) {
+        if (n % 2 == 0) {
+            // n이 짝수인 경우
+            n /= 2;
+        } else {
+            // n이 홀수인 경우
+            n = 3 * n + 1;
+        }
+        steps++;
+    }
+
+    return steps;
+}
 
 int main() {
-    int cnt=0;
-    int m,n;
-    cin>>m;
+    int m;
+    std::cin >> m;
 
-    for(int i=0;i<m;i++){
-        if(m%2==0){
-            m/=2;
-            cnt++;
-        }
-        else {m=3*m+1;
-        cnt++;}
-        if (m==1) cout<<cnt;
-        break;
-        cout<<"\n";
+    for (int i = 0; i < m; ++i) {
+        int n;
+        std::cin >> n;
+
+        int result = calculateStepsToReachOne(n);
+        std::cout<< result << "\n";
     }
-return 0;
+
+    return 0;
 }
