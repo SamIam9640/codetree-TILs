278fdc54169653d559ed6e83da63b0fa09cec0ab
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-06 10:08:25 +0900

    Title: 19단 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/19단 출력/nineteen-times-table.cpp
+++ 240306/19단 출력/nineteen-times-table.cpp
@@ -7,11 +7,11 @@ int main()
         for(int i=1;i<=19;i++){
             for(int j=1;j<=19;j++){
                 
-                if(j%2!=0&&j!=19) {cout<<i<<" * "<<j<<" = "<<i*j<<" / ";
+                if(j%2!=0) {cout<<i<<" * "<<j<<" = "<<i*j;
                 
              if(j==19) cout<<"\n";
                 }
-                else cout<<i<<" * "<<j<<" = "<<i*j<<"\n";
+                else cout<<" / "<<i<<" * "<<j<<" = "<<i*j<<"\n";
 
             }
         }
