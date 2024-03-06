db7d06e87b6913f17b3bdbba9c21124264e901d3
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-06 18:23:23 +0900

    Title: 완전수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240306/완전수/perfect-number.cpp
+++ 240306/완전수/perfect-number.cpp
@@ -1,27 +1,24 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    // 변수 선언 및 입력
- int a,b;
- cin >> a >>b;
-	int cnt=0;
-   
-	for(int i = a; i <= b; i++) {
-		int val =0;
-	
-		for (int s=1; s<=i-1; s++){
-           if ((i%s)==0) {
-             val +=s;
-           }
+int a,b;
+cin>>a>>b;
+int cnt=0;
+for(int i=a;i<=b;i++){
+    int sum=0;
+
+    for(int c=1;c<i;c++){
+        if((i%c)==0){
+            sum +=c;
+        }
     }
-            if(val ==i) {
-           cnt +=1;
-           }
+        if(sum==i){
+            cnt+=1;
+        }
     
-	}
-    	cout << cnt << endl;
-	
-	return 0;
+    
+
+}
+  cout<<cnt<<"\n";
+    return 0;
 }
