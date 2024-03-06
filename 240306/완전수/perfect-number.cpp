3f833b6e7cc13d17f85d151f99a054abf3d624fd
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-06 18:01:25 +0900

    Title: 완전수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/완전수/perfect-number.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int a,b;
+cin>>a>>b;
+int cnt=0;
+for(int i=a;i<=b;i++){
+    int sum=0;
+
+    for(int c=1;c<i;c++){
+        if((i%c)==0){
+            sum +=c;
+        }
+        if(sum==i){
+            cnt+=1;
+        }
+    }
+    
+
+}
+  cout<<cnt<<"\n";
+    return 0;
+}
