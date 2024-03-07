36ca741962122dd9c3af40267e0700bcb06414f8
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-07 12:15:45 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240307/특정 숫자 도달하기/reaching-specific-number.cpp
+++ 240307/특정 숫자 도달하기/reaching-specific-number.cpp
@@ -1,22 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
 int main() {
     int sum=0;
-    int arr[10];
-    int cnt=0;
+    float iphone=0;
+    int a[10];
+    for(int i =0; i<10;i++)
+    {
+        cin >>a[i];
+        if (a[i]<250)
+        {
+            sum+=a[i];
+            iphone +=1;
+        }
+        else
+        { 
+        break;
+        }
+    }    
+    
+    cout.precision(1);
+    cout<<fixed;
 
-for(int i=0;i<10;i++){
-    cin>>arr[10];
-    if(arr[i]<250){
-    sum+=arr[i];
-    cnt+=1;
-    }
-    else break;
-}
-cout.precision(1);
-cout<<fixed;
-
-    cout<<sum<<" "<<sum/cnt;
+    
+    cout <<sum<<' '<<sum/iphone;
+       
     return 0;
 }
