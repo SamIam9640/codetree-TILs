f69ac4da8f40e115e046d968461e787956ca21ce
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-08 23:45:55 +0900

    Title: 특정 위치의 문자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240308/특정 위치의 문자/char-in-specific-location.cpp
+++ 240308/특정 위치의 문자/char-in-specific-location.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main() {
-char n;
-cin>>n;
-char word[6]={'L','E','B','R','O','S'};
+    int idx=-1;
 
-int idx=-1;
+    char a;
+    cin>>a;
 
-for(int i=0;i<6;i++){
-    if(word[i]==n){
-        idx=i;
+    char word[6]={'L','E','B','R','O','S'};
+
+    for(int i=0;i<6;i++){
+        if(word[i]==a) {idx=i;}
     }
-}
-if(idx== -1){
-    cout<<"None";
-}
-else cout<<idx;
-return 0;
+
+    if(idx==-1) cout<<"None";
+    else cout<<idx;
+    return 0;
 }
