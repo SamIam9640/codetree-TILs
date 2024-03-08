e22e7cb40fccf8983ee1714b1f1ae9e4327d7ac1
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-08 13:10:07 +0900

    Title: 십의 자리 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/십의 자리 숫자/number-of-tens-digit.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[99];
+    int cnt_arr[10]={};
+
+    for(int i=0;i<99;i++){
+        cin>>arr[i];
+        if(arr[i]==0)
+        break;
+        if(arr[i]<10)
+        continue;
+        cnt_arr[arr[i]/10]++;
+}
+
+for(int i=1;i<10;i++){
+    cout<<i<<" - "<<cnt_arr[i]<<"\n";
+}
+return 0;
+}
