6e838315444908f66d2602ddda3aadf90cbdffb3
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-08 13:11:37 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[100];
+    int count_arr[10]={0, };
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        count_arr[arr[i]]++;
+    }
+    for(int i=1;i<=9;i++){
+        cout<<count_arr[i]<<"\n";
+    }
+    return 0;
+}
