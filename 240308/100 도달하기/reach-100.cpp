587bad49325326e00a4f4a9283d5e20aee335b26
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-08 09:16:28 +0900

    Title: 100 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240308/100 도달하기/reach-100.cpp
+++ 240308/100 도달하기/reach-100.cpp
@@ -1,22 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
 int main() {
-    int n;
-     cin>>n;
-    int cnt;
-    int arr[100]={1,n};
-   
+    // 입력받은 정수를 배열의 두번째 인덱스에 저장
+	int n;
+	cin >> n;
+	int arr[100] = {1, n};
+	int cnt = 1;
 
-    while(true){
-        cnt++;
-        arr[cnt]=arr[cnt-1]+arr[cnt-2];
-        if(arr[cnt]>100)
-        break;
+    // 배열을 전전항과 전항을 더한 수로 채워나가고, 그 값이 100을 초과하면 while문을 탈출
+    while(true) {
+		cnt++;
+		arr[cnt] = arr[cnt - 1] + arr[cnt - 2];
+		if(arr[cnt] > 100)
+			break;
+	}
+
+    // 100을 딱 초과한 항까지 배열의 인덱스에 저장된 값을 출력
+    for(int i = 0; i <= cnt; i++) {
+        cout << arr[i] << " ";
     }
-    for(int i=0;i<=cnt;i++){
-        cout<<arr[i]<<" ";
-    }
-    
+
     return 0;
 }
