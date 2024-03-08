6f5ca718890d3fb84be918a23fcf2cea02185068
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-08 23:59:54 +0900

    Title: 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240308/개수 세기/count-numbers.cpp
+++ 240308/개수 세기/count-numbers.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
-    int n,m;
-    int arr[100]={};
 
+    int n,m;
+
+    cin>>n>>m;
+
+    int arr[100]={};
     int cnt=0;
-   
-cin>>n>>m;
 
     for(int i=0;i<n;i++){
         cin>>arr[i];
         if(arr[i]==m) cnt++;
     }
-
     cout<<cnt;
     return 0;
 }
