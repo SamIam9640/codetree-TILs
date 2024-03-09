331e16545ac71111745206e6a052281fa33860aa
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-09 16:17:44 +0900

    Title: n개의 숫자 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
+++ 240309/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -2,14 +2,32 @@
 using namespace std;
 
 int main() {
-    int a[100],n;
+    int a[100],n,max1,max2;
 
     cin>>n;
+
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    sort(a,a+n,greater<int>());
 
-    cout<<a[0]<<" "<<a[1];
-    return 0;
+    if (a[0]>a[1]){
+        max1=a[0];
+        max2=a[1];
+    }
+    else {max1=a[1];
+    max2=a[0];}
+
+    for(int i=2;i<n;i++){
+        if(a[i]>=max1){
+            max2=max1;
+
+            max1=a[i];
+        }
+        else if(a[i]>max2){
+            max2=a[i];
+        }
+    }
+cout<<max1<<" "<<max2;
+return 0 ;
+
 }
