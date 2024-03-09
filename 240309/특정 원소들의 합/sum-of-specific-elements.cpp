c6410ab80066ff64d0f0725ca7735990af4f47b0
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-09 19:57:05 +0900

    Title: 특정 원소들의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/특정 원소들의 합/sum-of-specific-elements.cpp
+++ 240309/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -12,7 +12,7 @@ int main() {
     }
     for(int i=0;i<4;i++){
         for(int j=0;j<4;j++){
-            if(j>=i)
+            if(i>=j)
             {
                 sum+=arr_2d[i][j];
             }
