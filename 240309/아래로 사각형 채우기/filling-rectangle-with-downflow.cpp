b0882a3b911f79046302ba6ebf82767f3d381e0f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-09 23:06:23 +0900

    Title: 아래로 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
+++ 240309/아래로 사각형 채우기/filling-rectangle-with-downflow.cpp
@@ -10,7 +10,7 @@ int main() {
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            arr_2d[j][i]==cnt++;
+            arr_2d[j][i]=cnt++;
         }
     }
     for(int i=0;i<n;i++){
