fcef3253b25a8127de23c17e7faa868864342431
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-09 19:06:26 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240309/배열의 합/sum-of-array.cpp
+++ 240309/배열의 합/sum-of-array.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main(){
     int arr_2d[4][4];
 
-    for(int i=0;i<4;i++)
-    for(int j=0;j<4;j++)
-    cin>>arr_2d[i][j];
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cin>>arr_2d[i][j];
+        }
+
+    }
 
     for(int i=0;i<4;i++){
         int sum=0;
