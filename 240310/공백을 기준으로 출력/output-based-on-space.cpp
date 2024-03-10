3d742d515a2c15217f64f136423430c23259af7b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 13:30:48 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str1,str2;
+
+    getline(cin,str1);
+    getline(cin,str2);
+
+    int len1=str1.length();
+    int len2=str2.length();
+
+
+
+}
