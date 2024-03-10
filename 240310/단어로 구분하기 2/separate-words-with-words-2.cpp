9b2f337c87d67bc2cbe8c95b51c4fdf656361d3c
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 15:23:22 +0900

    Title: 단어로 구분하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str[10];
+    for(int i=0;i<10;i++){
+        cin>>str[i];
+    }
+    for(int i=0;i<10;i+=2){
+        cout<<str[i]<<"\n";
+    }
+    return 0;
+    }
