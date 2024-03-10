a50c173afb25eb9e1458de797641f7eab34290e6
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 13:01:08 +0900

    Title: 문자열 범위 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/문자열 범위 출력하기/print-string-in-range.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string str;
+
+    getline(cin,str);
+
+    for(int i=2;i<10;i++){
+        cout<<str[i];
+    }
+
+    return 0;
+}
