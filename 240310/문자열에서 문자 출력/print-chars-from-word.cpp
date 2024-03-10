d808bdf6c27b2d979790eed7b1112e564d776da0
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 16:20:34 +0900

    Title: 문자열에서 문자 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string arr;
+    cin>>arr;
+    int len=arr.length();
+
+    for(int i=0;i<len;i++){
+        cout<<arr[i]<<"\n";
+    }
+    return 0;
+}
