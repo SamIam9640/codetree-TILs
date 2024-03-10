496378c36a7c291cdf86587d2fc2b0e286fbb7bf
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 12:18:42 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    string str1, str2;
+
+    cin>>str1>>str2;
+
+    int len1=str1.length();
+    int len2=str2.length();
+
+    if(len1>len2) cout<<str1<<" "<<len1;
+    else cout<<str2<<" "<<len2;
+    return 0;
+}
