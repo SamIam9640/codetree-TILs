4b3636b6dd7040853f01de55674172bcf7015fb0
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 11:45:38 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/문자열의 길이 출력하기/print-string's-range.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string str1;
+    string str2;
+    cin>>str1;
+    cin>>str2;
+
+    int sum=0;
+    sum=str1.length()+str2.length();
+    cout<<sum;
+    return 0;
+}
