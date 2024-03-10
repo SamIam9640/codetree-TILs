c7d3b1e75ed5e4cb04762cae7302f532c1d5777d
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 11:31:37 +0900

    Title: 격자에 점 그리기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240310/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin>>n>>m;
+    int arr[10][10]={};
+
+    for(int i=0;i<m;i++){
+        int r,c;
+        cin>>r>>c;
+        arr[r][c]=
+        
+    }
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
