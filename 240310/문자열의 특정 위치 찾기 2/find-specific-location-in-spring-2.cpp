e4d6ca77cef746c4f4eb9ce936a5dd3f9021ad35
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 13:43:49 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.cpp
+++ 240310/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-
 int main() {
-
-    char a;
-    cin>>a;
-
-    string str[5]={"apple","banana","grape","blueberry","orange"};
-
 int cnt=0;
+char a;
+cin>>a;
+
+string str[5]={"apple","banana","grape","blueberry","orange"};
 
 for(int i=0;i<5;i++){
     if(str[i][2]==a||str[i][3]==a){
-        cout<<str[i]<<endl;
+        cout<<str[i]<<"\n";
         cnt++;
     }
 }
 cout<<cnt;
 return 0;
+
 }
