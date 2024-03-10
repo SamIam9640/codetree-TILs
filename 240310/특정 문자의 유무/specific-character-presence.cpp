e3d6139f60875f2d6ef5f5af870bce0cdc999c50
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 18:06:01 +0900

    Title: 특정 문자의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/특정 문자의 유무/specific-character-presence.cpp
+++ 240310/특정 문자의 유무/specific-character-presence.cpp
@@ -2,26 +2,19 @@
 using namespace std;
 
 int main() {
+    string str;
 
-    string s;
-    cin>>s;
-    int length=s.length();
-    bool exists =false;
+    cin>>str;
 
-    for(int i=0;i<length-1;i++){
-        if(s[i]=='e'&&s[i+1]=='e'){
-            exists=true;
-        }
-    }
-    if(exists==true) cout<<"Yes ";
-    else cout<<"No ";
+    if(str.find("ee") != string::npos)
+    cout<<"Yes ";
+    else
+    cout<<"No ";
 
-    for(int i=0;i<length-1;i++){
-        if(s[i]=='a'&&s[i+1]=='b'){
-            exists=true;
-        }
-    }
-    if(exists==true) cout<<"Yes";
-    else cout<<"No";
-    return 0;
+    if(str.find("ab")!=string::npos)
+    cout<<"Yes";
+    else
+    cout<<"No";
+
+    return 0; 
 }
