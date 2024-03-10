45681965fe1ec6fc91e2df41daf4a3ef3791d6ac
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 11:01:12 +0900

    Title: 배열로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/배열로 사각형 만들기/print-array-in-rectangle.cpp
+++ 240310/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -17,7 +17,7 @@ int main() {
         }
 
         for(int i=0;i<n;i++){
-            for(int j=1;j<n;j++){
+            for(int j=0;j<n;j++){
                 cout<<arr_2d[i][j]<<" ";
             }
             cout<<"\n";
