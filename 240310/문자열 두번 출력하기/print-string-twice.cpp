7b6c98cd99efe3dd6d8372bb293479184bbf94fc
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-10 11:42:06 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/문자열 두번 출력하기/print-string-twice.cpp
+++ 240310/문자열 두번 출력하기/print-string-twice.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-    cout<<"LeeBrosCode"<<"\n";
-    cout<<"LeeBrosCode"<<"\n";
+    string str;
+    cin>>str;
+    cout<<str<<"\n";
+    cout<<str<<"\n";
     return 0;
 }
