8121c3be6b26f4f464a06f8adc93b064eb5d61c3
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-11 13:37:35 +0900

    Title: 대문자로 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240311/대문자로 출력하기/print-in-capital.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+
+    cin>>str;
+
+    int len=str.length();
+    for(int i=0;i<len;i++){
+        if((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z'))
+    cout<<(char)toupper(str[i]);
+    }
+    return 0;
+    }
