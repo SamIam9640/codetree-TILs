4e9537be750e9586f74e38da8ce5b4230e9b9114
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-11 11:52:50 +0900

    Title: 아스키코드의 합과 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
+++ 240311/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+char a,b;
+cin>>a>>b;
 
-cout<<(int)'L'+(int)'e' <<" "<<(int)'e'-(int)'L';
+int a_num=(int)a;
+int b_num=(int)b;
+cout<<a_num+b_num<<" ";
+
+if(a_num>b_num){
+    cout<<a_num-b_num;
+}
+else
+cout<<b_num-a_num;
+
 return 0;
 }
