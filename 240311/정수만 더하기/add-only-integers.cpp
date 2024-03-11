b31bdb8e51a8c82ea2dd25832871587df2db8c12
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-11 15:36:10 +0900

    Title: 정수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240311/정수만 더하기/add-only-integers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    cin>>str;
+    int sum=0;
+
+    int len=str.length();
+    for(int i=0;i<len;i++){
+        if(str[i]>='0'&&str[i]<='9')
+        {
+            sum+=(str[i]-'0');
+        }
+    }
+    cout<<sum;
+    return 0;
+}
