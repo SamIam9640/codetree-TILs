d5cad7147c08270c99f0b7b3fb4856aae8a89445
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-11 13:48:21 +0900

    Title: 소문자와 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/소문자와 숫자/letter-and-number.cpp
+++ 240311/소문자와 숫자/letter-and-number.cpp
@@ -6,13 +6,13 @@ int main() {
     cin>>str;
 
     int len=str.length();
+
     for(int i=0;i<len;i++){
         if(str[i]>='A'&&str[i]<='Z'||str[i]>='a'&&str[i]<='z')
         cout<<(char)tolower(str[i]);
-        if(str[i]>='0'&&str[i]<='9'){
-            cout<<str[i];
-        }
+
+        if(str[i]>='0'&&str[i]<='9')
+        cout<<str[i];
     }
     return 0;
-    
 }
