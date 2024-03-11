0bcfe8eba461769a1adba97636a152a9600eb5b8
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-11 13:14:47 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240311/그 다음 알파벳/next-alphabet.cpp
+++ 240311/그 다음 알파벳/next-alphabet.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
     char c;
     cin>>c;
-    
-    if(c=='z')
-    cout<<"a";
-    else{
-    cout<<(char)(c+1);}
+
+    if(c=='z') c='a';
+    else cout<<(char)(c+1);
     return 0;
 }
