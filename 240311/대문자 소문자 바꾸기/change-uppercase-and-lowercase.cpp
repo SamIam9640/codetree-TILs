46e4efcd9055f203df221631951c87948289c773
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-11 15:41:32 +0900

    Title: 대문자 소문자 바꾸기, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240311/대문자 소문자 바꾸기/change-uppercase-and-lowercase.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    cin>>str;
+
+    int len=str.length();
+    for(int i=0;i<len;i++){
+        if(str[i]>='A'&&str[i]<='Z')
+        {cout<<(char)tolower(str[i]);}
+        if(str[i]>='a'&&str[i]<='z')
+        {
+            cout<<(char)toupper(str[i]);
+        }
+        return 0;
+    }
+}
