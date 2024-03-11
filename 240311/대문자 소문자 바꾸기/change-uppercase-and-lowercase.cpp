0451a5991a2eddfe3cfd088bcea367f11022ca5b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-11 15:41:37 +0900

    Title: 대문자 소문자 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/대문자 소문자 바꾸기/change-uppercase-and-lowercase.cpp
+++ 240311/대문자 소문자 바꾸기/change-uppercase-and-lowercase.cpp
@@ -1,18 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cctype>
 using namespace std;
 
 int main() {
     string str;
-    cin>>str;
+    cin >> str;
 
-    int len=str.length();
-    for(int i=0;i<len;i++){
-        if(str[i]>='A'&&str[i]<='Z')
-        {cout<<(char)tolower(str[i]);}
-        if(str[i]>='a'&&str[i]<='z')
-        {
-            cout<<(char)toupper(str[i]);
+    int len = str.length();
+    for (int i = 0; i < len; i++) {
+        if (isupper(str[i])) {
+            cout << (char)tolower(str[i]);
+        } else if (islower(str[i])) {
+            cout << (char)toupper(str[i]);
+        } else {
+            cout << str[i];  // 처리할 필요 없는 문자는 그대로 출력
         }
-        return 0;
     }
+
+    return 0;
 }
