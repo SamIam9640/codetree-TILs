008bcdaf6ed69296494851d22de617e099ef2ed9
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-12 09:25:46 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Print5Stars(){
+    for(int i=0;i<10;i++){
+        cout<<"*";
+    }
+    cout<<"\n";
+}
+int main(){
+for(int i=0;i<5;i++){
+    Print5Stars();
+}
+return 0;
+}
