69e003c9be32959f4722c243e906ec5182128b3c
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-12 09:55:46 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void PrintRect(int n,int m){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<"1";
+        }
+        cout<<"\n";
+    }
+}
+
+int main() {
+    int row_num,col_num;
+    cin>>row_num>>col_num;
+
+    PrintRect(row_num,col_num);
+    return 0;
+}
