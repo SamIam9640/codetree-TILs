552185c5c0d38ad488869cf1fe62a8aaeaed4850
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-12 13:07:35 +0900

    Title: 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240312/정수의 최솟값/minimum-value-of-an-integer.cpp
+++ 240312/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Add(int a,int b,int c){
-return min(min(a,b),c);
+int Min(int a, int b,int c){
+    return min(min(a,b),c);
 }
 
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
 
-    cout<<Add(a,b,c);
-    return 0;
+    cout<<Min(a,b,c);
+    return 0 ;
 }
