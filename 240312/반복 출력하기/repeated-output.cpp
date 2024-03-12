f40e2609c8f449eb8e38dbcc77d0e407e67aaa72
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-12 09:45:22 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void PrintNLines(int n){
+    for(int i=0;i<n;i++){
+        cout<<"12345^&*()_";
+           cout<<"\n";
+    }
+ 
+}
+
+int main() {
+    int row_num;
+    cin>>row_num;
+
+    PrintNLines(row_num);
+    
+    return 0;
+}
