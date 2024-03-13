c7fbd4c8da7b6be69dd8d1c184f98f4bb755849f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-13 12:36:22 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240313/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,33 @@
+bool 두개
+int main
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool isExist(int n){
+
+
+    
+}
+bool Exists(){
+    for(int i=1;i<=12;i++){
+        for(int j=1;i<=31;j++){
+            if(isExist(i)) return true;
+        }
+    }
+    return false;
+}
+
+
+
+int main(){
+    int m,d;
+    cin>>m>>d;
+
+    if(Exists())
+    cout<<"Yes";
+    else cout<<"No";
+}
