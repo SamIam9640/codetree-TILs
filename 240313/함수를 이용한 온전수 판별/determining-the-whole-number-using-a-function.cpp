b94d8ffc6e4b056aca275196df43768ebab81d46
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-13 09:37:05 +0900

    Title: 함수를 이용한 온전수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isOn(int n){
+    if(n%2==0) 
+    return false;
+    if(n%10==5)
+    return false;
+    if(n%3==0&&n%9!=0)
+    return false;
+    return true;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int cnt=0;
+
+    for(int i=a;i<=b;i++){
+        if(isOn(i)) cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}
