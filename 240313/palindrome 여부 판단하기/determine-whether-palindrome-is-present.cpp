752a133db99799b54cbb2c0bbcdf73b5ae4f33e4
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-13 22:21:02 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+
+bool Palindrome(string &s){
+    for(int i=0;i<(int)s.size();i++){
+        if(s[i]!=s[(int)s.size()-i-1])
+        return false;
+
+        return true;
+    }
+}
+
+int main(){
+    cin>>str;
+
+    if(Palindrome(str))
+    cout<<"Yes";
+    else
+    cout<<"No";
+
+    return 0;
+}
