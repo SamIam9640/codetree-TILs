4b962ca8d9606c1f729f5ae36bac480948c2fa49
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-13 09:48:07 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool Both(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0) return false;
+    }
+
+    if(((n%10)+(n/10))%2!=0) return false;
+    return true;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int cnt=0;
+
+    for(int i=a;i<=b;i++){
+if(Both(i)) cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}
