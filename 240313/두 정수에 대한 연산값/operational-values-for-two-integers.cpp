33d7d2ddbd764b0f2cee8995c33d6c4f53d50372
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-13 22:38:32 +0900

    Title: 두 정수에 대한 연산값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Plus(int &a,int &b){
+    if(a>b){a+=25;
+     b*=2;}
+     else {a*=2;
+     b+=25;}
+}
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    Plus(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
