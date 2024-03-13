bfb0ad32deb600f9e57294445df0bbd681b4761e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-13 23:30:40 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Hello(int &a,int &b){
+    if(a>b)
+    {a*=2;b+=10;};
+    if(b>a)
+    {a+=10;b*=2;};
+    return ;
+}
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    Hello(a,b);
+    cout<<a<< " "<<b;
+    return 0;
+}
