96e1ab7c8a38c093014edf56a19749973d180bf5
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-14 13:46:08 +0900

    Title: 반복 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240314/반복 출력하기 2/repeated-output-2.cpp
+++ 240314/반복 출력하기 2/repeated-output-2.cpp
@@ -5,15 +5,15 @@ void PrintText(int n){
     if(n==0) return;
 
     PrintText(n-1);
-    
+
     cout<<"HelloWorld";
     cout<<"\n";
 }
 
 int main(){
-
     int n;
     cin>>n;
+
     PrintText(n);
     return 0;
 }
