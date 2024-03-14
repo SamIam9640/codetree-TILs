c34182921e7491d86a41f3c9aa44a02692125fdc
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-14 14:18:47 +0900

    Title: 숫자 차례로 출력하기, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240314/숫자 차례로 출력하기/to-output-numerically.cpp
+++ 240314/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -1,26 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void PrintNum1(int n){
+
+void PrintText1(int n){
     if(n==0) return;
-PrintNum1(n-1);
-cout<<n<<" ";
+
+    PrintText1(n-1);
+    cout<<n<<" ";
 }
 
-void PrintNum2(int n){
+void PrintText2(int n){
     if(n==0) return;
-
-    cout<<n<<" ";
-    PrintNum2(n-1);
+cout<<n<<" ";
+    PrintText2(n-1);
 }
 
 int main(){
     int n;
     cin>>n;
-    PrintNum1(n);
-    cout<<"\n";
-    PrintNum2(n);
-    
 
+    PrintText1(n);
+    cout<<"\n";
+    PrintText2(n);
     return 0;
 }
