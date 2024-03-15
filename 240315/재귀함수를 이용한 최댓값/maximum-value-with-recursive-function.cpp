e1b9448478a9a32a8e91f93ba9a9e0dd41d8bb45
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-15 09:00:03 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240315/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
+++ 240315/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
-
 #define MAX_N 100
+
 using namespace std;
 
 int arr[MAX_N];
 
-int MaxValue(int a){
-    if(a==0) return arr[0];
+int MaxValue(int n){
+    if(n==0) return arr[0];
 
-    return max(MaxValue(a-1),arr[a]);
+    return max(MaxValue(n-1),arr[n]);
 }
 
 int main(){
-
     int n;
     cin>>n;
 
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
 
     cout<<MaxValue(n-1);
     return 0;
