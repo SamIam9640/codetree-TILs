6a781a79ce1ed46da1b11c7f721749a02b4430b3
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-15 09:45:27 +0900

    Title: 큰 숫자 자리수의 합, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240315/큰 숫자 자리수의 합/sum-of-large-numeric-digits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int DigitSum(int n){
+    if(n<10) return n;
+
+    return DigitSum(n/10)+n%10;
+}
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    cout<<DigitSum(a*b*c);
+    return 0;
+}
