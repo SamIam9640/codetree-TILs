6d65b1e8a9a1d50e701dfed27ae705bcaa49e064
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-15 13:14:35 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240315/재귀함수를 이용한 3n + 1 수열/3n+1-sequence-with-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int CountNumber(int a){
+    if(a==1) return 0;
+
+    if(a%2==0)
+    return CountNumber(a/2)+1;
+    else
+    return CountNumber(3*a+1)+1;
+}
+
+int main() {
+    int n;
+    cin>>n;
+
+    cout<<CountNumber(n);
+    return 0;
+}
