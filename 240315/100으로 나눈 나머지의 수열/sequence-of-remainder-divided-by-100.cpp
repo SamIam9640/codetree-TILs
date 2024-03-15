50d0fcbe6401e1b4c34c9ae572034e33f9da9294
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-15 13:45:13 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240315/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.cpp
+++ 240315/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int GetNum(int a){
-    if (a==1) return 2;
-    if (a==2) return 4;
+int GetNum(int n){
+    if (n==1) return 2;
+    if (n==2) return 4;
 
-    return (GetNum(a-1)*GetNum(a-2))%100;
+    return (GetNum(n-1)*GetNum(n-2))%100;
 }
 
 int main(){
