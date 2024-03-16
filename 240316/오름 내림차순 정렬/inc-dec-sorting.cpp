f7e84a39ca89f10371a72fcb2aecf66e68d8a5c0
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-16 11:56:32 +0900

    Title: 오름 내림차순 정렬, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240316/오름 내림차순 정렬/inc-dec-sorting.cpp
+++ 240316/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -2,28 +2,25 @@
 #define MAX_N 100
 using namespace std;
 
-int arr[MAX_N];
+int num[MAX_N];
 int main(){
-
-    
     int n;
     cin>>n;
 
     for(int i=0;i<n;i++){
-        cin>>arr[i];
+        cin>>num[i];
     }
 
-    sort(arr,arr+n);
+    sort(num,num+n);
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+        cout<<num[i]<<" ";
     }
     cout<<"\n";
 
-sort(arr,arr+n,greater<int>());
-
-for(int i=0;i<n;i++){
-    cout<<arr[i]<<" ";
-}
- cout<<"\n";
-return 0;
+    sort(num,num+n,greater<int>());
+    for(int i=0;i<n;i++){
+        cout<<num[i]<<" ";
+    }
+    cout<<"\n";
+    return 0;
 }
