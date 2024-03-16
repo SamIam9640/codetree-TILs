9e09f138834d4c736a589b6a6ad1ff61175d4b9c
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-16 12:28:01 +0900

    Title: 단어 정렬, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240316/단어 정렬/sorting-words.cpp
+++ 240316/단어 정렬/sorting-words.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
-#define MAX_N 100
 using namespace std;
-string words[MAX_N]={};
-int main(){
 
+#define MAX_N 100
+
+string words_list[MAX_N];
+int main(){
     int n;
     cin>>n;
 
-    for(int i=0;i<n;i++)
-    cin>>words[i];
-    sort(words,words+n);
     for(int i=0;i<n;i++){
-        cout<<words[i]<<"\n";
+        cin>>words_list[i];
+    }
+
+    sort(words_list,words_list+n);
+    for(int i=0;i<n;i++){
+        cout<<words_list[i]<<"\n";
     }
     return 0;
 }
