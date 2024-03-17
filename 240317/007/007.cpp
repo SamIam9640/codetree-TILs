cb98d1963d333ff69bbd6a970d5ddbf9b9b4715b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-17 09:54:24 +0900

    Title: 007, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240317/007/007.cpp
+++ 240317/007/007.cpp
@@ -1,32 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 class Spy{
     public:
-    string secret_code;
-    char meeting_point;
+    string s_code;
+    char m_point;
     int time;
 
-    Spy(string secret_code,char meeting_point,int time){
-        this->secret_code=secret_code;
-        this->meeting_point=meeting_point;
-        this->time=time;
+    Spy(string s_code,char m_point,int time){
+    this->s_code=s_code;
+    this->m_point=m_point;
+    this->time=time;
     }
 };
-
 
 int main(){
     string s_code;
     char m_point;
     int time;
-
     cin>>s_code>>m_point>>time;
 
     Spy s=Spy(s_code,m_point,time);
 
-    cout<<"secret code : "<<s.secret_code<<"\n";
-    cout<<"meeting point : "<<s.meeting_point<<"\n";
-    cout<<"time : "<<s.time<<"\n";
+    cout<<"secret code : "<<s_code<<"\n";
+    cout<<"meeting point : "<<m_point<<"\n";
+    cout<<"time : "<<time;
     return 0;
 }
