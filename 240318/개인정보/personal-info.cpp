c6bd4a0be53d205b80d5f4d6bb9a2204d92a16b6
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-18 18:04:48 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/개인정보/personal-info.cpp
+++ 240318/개인정보/personal-info.cpp
@@ -6,30 +6,55 @@ using namespace std;
 class Person{
     public :
     string name;
-    int height, weight;
-    Person(string name,int height,int weight){    
+    int height;
+    double weight;
+    Person(string name,int height,double weight){   
+        this->name=name;
+        this->height=height;
+        this->weight=weight; 
     }
     Person (){}
 };
 
-bool cmp(Person a,Person b){
+bool cmp1(Person a,Person b){
     return a.height>b.height;
+}
+bool cmp2(Person a,Person b){
+    return a.name<b.name;
 }
 
 Person people[MAXN];
 
 int main(){
-    for(int i=0;i<5;i++){
+    for(int i=0;i<MAXN;i++){
         string name;
-        int height,weight;
+        int height;
+        double weight;
 cin>>name>>height>>weight;
 people[i]=Person(name,height,weight);
     }
-sort(people,people+5,cmp);
 
-for(int i=0;i<5;i++){
-    cout<<people[i].name<<" "<<people[i].height<< " "
-    <<people[i].weight<<"\n";
+    cout<<fixed;
+    cout.precision(1);
+
+
+sort(people,people+MAXN,cmp2);
+cout<<"name"<<"\n";
+for(int i=0;i<MAXN;i++){
+     cout<<people[i].name<<" ";
+    cout<<people[i].height<< " ";
+    cout<<people[i].weight<<"\n";
+}
+cout<<"\n";
+
+sort(people,people+5,cmp1);
+
+cout<<"height"<<"\n";
+
+for(int i=0;i<MAXN;i++){
+    cout<<people[i].name<<" ";
+    cout<<people[i].height<< " ";
+    cout<<people[i].weight<<"\n";
 }
 return 0;
 }
