ef3d233604def524e2c7516fb82301dc4d14f7bb
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-18 15:35:19 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/총점 비교/compare-total-points.cpp
+++ 240318/총점 비교/compare-total-points.cpp
@@ -5,39 +5,40 @@ using namespace std;
 class Student{
     public:
     string name;
-    int kor, eng, math;
-
-    Student(string name, int kor, int eng, int math){
+    int kor,eng,math;
+    Student (string name,int kor,int eng,int math){
         this->name=name;
         this->kor=kor;
         this->eng=eng;
         this->math=math;
     }
-    Student() {}
+    Student (){}
 };
 
 bool cmp(Student a,Student b){
     return a.kor+a.eng+a.math<
     b.kor+b.eng+b.math;
 }
+
 Student students[MAXN];
+
 int main(){
     int n;
     cin>>n;
 
-    string name;
-    int kor,eng,math;
     for(int i=0;i<n;i++){
+        string name;
+        int kor,eng,math;
+
         cin>>name>>kor>>eng>>math;
-        students[i]=Student(name,kor,eng,math);
+        students[i] = Student(name, kor, eng, math);
     }
 
     sort(students,students+n,cmp);
 
     for(int i=0;i<n;i++){
-        cout<<students[i].name<<" "<<students[i].kor<<" "<<
-        students[i].eng<<" "<<students[i].math<<"\n";
+        cout<<students[i].name<<" "<<students[i].kor<<" "<<students[i].eng<<" "
+        <<students[i].math<<"\n";
     }
-
-return 0;
+    return 0;
 }
