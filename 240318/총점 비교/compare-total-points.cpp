417e15678dfb40ad2f3bde3a8e3f52c2f3b92180
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-18 15:14:32 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240318/총점 비교/compare-total-points.cpp
+++ 240318/총점 비교/compare-total-points.cpp
@@ -1,35 +1,43 @@
 #include <bits/stdc++.h>
+#define MAXN 10
 using namespace std;
 
 class Student{
     public:
-    int kor,eng, math;
+    string name;
+    int kor, eng, math;
 
-    Student (int kor,int eng,int math){
+    Student(string name, int kor, int eng, int math){
+        this->name=name;
         this->kor=kor;
         this->eng=eng;
         this->math=math;
     }
+    Student() {}
 };
 
 bool cmp(Student a,Student b){
-    return a.kor+a.eng+a.math<b.kor+b.eng+b.math;
+    return a.kor+a.eng+a.math<
+    b.kor+b.eng+b.math;
 }
-
+Student students[MAXN];
 int main(){
-    Student students[5]={
-        Student(90, 80, 90), // 첫 번째 학생
-        Student(20, 80, 80), // 두 번째 학생
-        Student(90, 30, 60), // 세 번째 학생
-        Student(60, 10, 50), // 네 번째 학생
-        Student(80, 20, 10)
-    };
+    int n;
+    cin>>n;
 
-    sort(students,students+5,cmp);
-
-    for(int i=0;i<5;i++){
-        cout<<students[i].kor<<" "<<students[i].eng<<" "<<students[i].math<<"\n";
-
-        return 0;
+    string name;
+    int kor,eng,math;
+    for(int i=0;i<n;i++){
+        cin>>name>>kor>>eng>>math;
+        students[i]=Student(name,kor,eng,math);
     }
+
+    sort(students,students+n,cmp);
+
+    for(int i=0;i<n;i++){
+        cout<<students[i].name<<" "<<students[i].kor<<" "<<
+        students[i].eng<<" "<<students[i].math<<"\n";
+    }
+
+return 0;
 }
