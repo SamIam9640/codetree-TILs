ff0f67426b5ff57abe51431c99b6fbe6033a2d5e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-19 13:14:01 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240319/진수 to 진수/transformation-of-number-system.cpp
+++ 240319/진수 to 진수/transformation-of-number-system.cpp
@@ -1,32 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main(){
-    int a,b;
-    string str;
-    int digits[20]={};
-    int cnt=0;
-
-    cin>>a>>b>>str;
-
-    int num=0;
-
-    for(int i=0;i<(int)str.size();i++)
-    num=num*2+(str[i]-'0');
-
-    
-    while(true){
-        if(num<b){
-            digits[cnt++]=num;
-            break;
-        }
-        digits[cnt++]=num%b;
-        num/=b;
+// a진수를 10진수로 변환하는 함수
+int toDecimal(int n, int base) {
+    int result = 0, power = 1;
+    while (n > 0) {
+        result += (n % 10) * power;
+        power *= base;
+        n /= 10;
     }
-    for(int i=cnt-1;i>=0;i--){
-        cout<<digits[i];
-    }
+    return result;
+}
 
-return 0;
+// 10진수를 b진수로 변환하는 함수
+vector<int> toBase(int n, int base) {
+    vector<int> result;
+    while (n > 0) {
+        result.push_back(n % base);
+        n /= base;
+    }
+    reverse(result.begin(), result.end()); // 역순으로 변환
+    return result;
+}
+
+int main() {
+    int a, b, n;
+   
+    cin >> a >> b >> n;
+
+    // a진수를 10진수로 변환
+    int decimal = toDecimal(n, a);
+
+    // 10진수를 b진수로 변환
+    vector<int> result = toBase(decimal, b);
+
+    // 결과 출력
     
+    for (int digit : result) {
+        cout << digit;
+    }
+    cout << endl;
+
+    return 0;
 }
