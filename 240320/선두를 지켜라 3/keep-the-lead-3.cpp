4ea9a825ef46335be9403d9cda395a10c0015289
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-20 20:06:53 +0900

    Title: 선두를 지켜라 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240320/선두를 지켜라 3/keep-the-lead-3.cpp
+++ 240320/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -1,48 +1,55 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-#define MAXT 1000000
-int n,m;
-int pos_a[MAXT+1],pos_b[MAXT+1];
 
-int main(){
-    cin>>n>>m;
+int main() {
+    int n, m;
+    cin >> n >> m;
 
-    int time_a=1;
-    for(int i=0;i<n;i++){
-        int v,t;
-        cin>>v>>t;
+    vector<pair<int, int>> movements_a, movements_b;
 
-        while(t--){
-            pos_a[time_a]=pos_a[time_a-1]+v;
-            time_a++;
+    // A의 움직임 정보 입력
+    for (int i = 0; i < n; ++i) {
+        int v, t;
+        cin >> v >> t;
+        movements_a.push_back({v, t});
+    }
+
+    // B의 움직임 정보 입력
+    for (int i = 0; i < m; ++i) {
+        int v, t;
+        cin >> v >> t;
+        movements_b.push_back({v, t});
+    }
+
+    int leader_a = 1, leader_b = 1; // 초기에 A와 B가 선두로 시작함
+    int changes = 0;
+
+    // 매 시간대마다 A와 B의 상대적인 위치 비교
+    for (int time = 1; time <= 1000; ++time) {
+        // A와 B의 위치 계산
+        int pos_a = 0, pos_b = 0;
+        for (auto movement : movements_a) {
+            pos_a += movement.first * min(time, movement.second); // 최대 t초 이동
+        }
+        for (auto movement : movements_b) {
+            pos_b += movement.first * min(time, movement.second); // 최대 t초 이동
+        }
+
+        // 선두 확인 및 변경
+        if (pos_a > pos_b && leader_a != 1) {
+            ++changes;
+            leader_a = 1;
+            leader_b = 0;
+        } else if (pos_b > pos_a && leader_b != 1) {
+            ++changes;
+            leader_a = 0;
+            leader_b = 1;
         }
     }
-    int time_b=1;
-    for(int i=0;i<m;i++){
-        int v,t;
-        cin>>v>>t;
 
-        while(t--){
-            pos_b[time_b]=pos_b[time_b-1]+v;
-            time_b++;
-        }
-    }
-int leader=0,ans=0;
-for(int i=1;i<time_a;i++){
-    if(pos_a[i]>pos_b[i]){
-        if(leader==2)
-        ans++;
-    
-    leader=1;
-}
-else if(pos_a[i]<pos_b[i]){
-    if(leader==1)
-    ans++;
+    cout << changes << endl;
 
-    leader=2;
-}
-}
-
-cout<<ans;
-return 0;
+    return 0;
 }
