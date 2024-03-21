985847a8fda582d5ef2902d6faf7810946a2449c
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-21 23:36:57 +0900

    Title: 작은 구슬의 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240321/작은 구슬의 이동/small-marble-movement.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ASCII_NUM 128
+#define Dir_num 4
+
+using namespace std;
+
+int n,t;
+int x,y,dir;
+int mapper[ASCII_NUM];
+
+int dx[Dir_num] ={0,1,-1,0};
+int dy[Dir_num] ={1,0,0,-1};
+
+bool InRange(int x,int y){
+    return 0<=x&&x<n&&0<=y&&y<n;
+}
+
+void Simulate() {
+    while(t--) {
+        int nx=x+dx[dir], ny=y+dy[dir];
+
+        if(InRange(nx,ny)){
+            x=nx,y=ny;
+        }
+        else
+        dir=3-dir;
+    }
+}
+
+int main(){
+    cin>>n>>t;
+
+    mapper['R']=0;
+    mapper['D']=1;
+    mapper['U']=2;
+    mapper['L']=3;
+
+    char c_dir;
+    cin>>x>>y>>c_dir;
+    x--;y--;dir=mapper[c_dir];
+
+    Simulate();
+
+    cout<<x+1<<" "<<y+1;
+    return 0;
+}
