1ac9fe9215e0ed39f737dbfef32b1a72ec049dfc
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-24 18:19:39 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240324/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define MAXN 100
+using namespace std;
+
+int n;
+int x[MAXN],y[MAXN];
+
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++)
+    cin>>x[i]>>y[i];
+
+    int ans=INT_MAX;
+    for(int i=1;i<n-1;i++){
+        int dist=0;
+        int prev_idx=0;
+        for(int j=1;j<n;j++){
+            if(j==i) continue;
+            dist +=abs(x[prev_idx]-x[j])+abs(y[prev_idx]-y[j]);
+            prev_idx=j;
+        }
+        ans=min(ans,dist);
+    }
+    cout<<ans;
+    return 0;
+}
