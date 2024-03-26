c433ff1053249e361e1d55ca4e3962ca9e8b28d4
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-26 12:28:42 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
+++ 240326/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define MAXN 100
 
 int n;
 int arr[MAXN];
-
 int main(){
     cin>>n;
 
-    for(int i=0;i<n;i++)
-    cin>>arr[i];
-
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
     int ans=INT_MIN;
     for(int i=0;i<n;i++)
     for(int j=i+2;j<n;j++)
