32ec175dbec7e115171f4b6a4e191817226970a2
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-26 20:39:21 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240326/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a,b,c;
+
+int main(){
+    cin>>n;
+
+    cin>>a>>b>>c;
+    int cnt=0;
+
+    for(int i=1;i<=n;i++)
+    for(int j=1;j<=n;j++)
+    for(int k=1;k<=n;k++)
+    if(abs(a-i)<=2||abs(b-j)<=2||abs(c-k)<=2)
+    cnt++;
+
+    cout<<cnt;
+    return 0;
+}
