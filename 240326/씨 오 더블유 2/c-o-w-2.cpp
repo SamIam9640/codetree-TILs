b29b592042d3b829daf412991130a94831299d17
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-26 12:35:48 +0900

    Title: 씨 오 더블유 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240326/씨 오 더블유 2/c-o-w-2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAXN 100
+
+int n;
+string arr[MAXN];
+
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+
+    int cnt=0;
+    for(int i=0;i<n;i++)
+    for(int j=i+1;j<n;j++)
+    for(int k=j+1;k<n;k++)
+    if(arr[i]=='C'&&arr[i]=='O'&&arr[i]=='W')
+    cnt++;
+
+    cout<<cnt;
+    return 0;
+}
