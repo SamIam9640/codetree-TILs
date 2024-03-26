57ce434b1d193db8493e1930a5f132633d02ffdd
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-26 10:06:30 +0900

    Title: 원 모양으로 되어있는 방, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
+++ 240326/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -1,25 +1,34 @@
-#include <bits/stdc++.h>
-#define MAXN 1003
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <cstdlib>
+
+#define MAX_N 1003
 
 using namespace std;
 
 int n;
-int a[MAXN];
+int a[MAX_N];
 
-int main(){
-    cin>>n;
-    for(int i=0;i<n;i++)
-    cin>>a[i];
-
-    int ans=INT_MAX;
-    for(int i=0;i<n;i++){
-        int sum_dist=0;
-        for(int j=0;j<n;j++){
-            int dist=(j+n-1)%n;
-            sum_dist +=dist *a[j];
-        }
-        ans=min(ans,sum_dist);
+int main() {
+    // 입력
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    // i번째 방에서 출발했을 경우의 결과를 구해줍니다.
+    int ans = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        int sum_dist = 0;
+        for(int j = 0; j < n; j++) {
+			int dist = (j + n - i) % n;
+			sum_dist += dist * a[j];
+		}
+        
+        // 가능한 거리의 합 중 최솟값을 구해줍니다.
+        ans = min(ans, sum_dist);
     }
-    cout<<ans;
+    
+    cout << ans;
     return 0;
 }
