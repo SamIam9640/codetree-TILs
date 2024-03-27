2d755b07982ec46927c3f4ee90b113161245e84d
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-27 15:50:06 +0900

    Title: 삼각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240327/삼각형 만들기/create-triangle.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define MAX_N 10000
+using namespace std;
+
+int n;
+int x[MAX_N],y[MAX_N];
+
+int area(int i,int j,int k){
+    return abs((x[i]*y[j]+x[j]*y[k]+x[k]*y[i])-
+    (x[j]*y[i]+x[k]*y[j]+x[i]*y[k]));
+}
+
+int main(){
+
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    cin>>x[i]>>y[i];
+
+    int max_area=0;
+    for(int i=0;i<n;i++)
+    for(int j=i+1;j<n;j++)
+    for(int k=j+1;k<n;k++)
+    if((x[i]==x[j]||x[i]==x[k]||x[j]==x[k])&&
+    (y[i]==y[j]||y[i]==y[k]||y[j]==y[k]))
+    max_area=max(max_area,area(i,j,k));
+
+    cout<<max_area;
+    return 0;
+}
