e334c6f5a38380cf89ce99cfbd2a6ec9e7e59927
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-27 13:43:40 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240327/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
+++ 240327/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
+#define MAX_N 100
+#define MAX_NUM 40000
+
 using namespace std;
 
-int n=5;
-int segmetns[5][2]={{1,3},{2,4},{5,8},{6,9},{7,10}};
+int n;
+int x[MAX_N],y[MAX_N];
 
-int main(){
-    int ans=100;
+int main() {
+
+    cin>>n;
+    for(int i=0;i<n;i++)
+    cin>>x[i]>>y[i];
+
+    int ans=INT_MAX;
+
     for(int i=0;i<n;i++){
-        int counting[11]={};
+        int min_x=MAX_NUM,max_x=1;
+        int min_y=MAX_NUM,max_y=1;
         for(int j=0;j<n;j++){
-            if(j==i)
-            continue;
+            if(j==i) continue;
 
-            int x1=segments[j][0], x2=segments[j][1];
-            for(int k=x1;k<=x2;k++)
-            counting[k]++;
+            min_x=min(min_x,x[j]);
+            max_x=max(max_x,x[j]);
+            min_y=min(min_y,y[j]);
+            max_y=max(max_y,y[j]);
         }
-        int max_cnt=0;
-        for(int j=0;j<11;j++)
-        max_cnt=max(max_cnt,counting[j]);
-        ans=min(ans,max_cnt);
+        ans=min(ans,(max_x-min_x)*(max_y-min_y));
     }
     cout<<ans;
     return 0;
