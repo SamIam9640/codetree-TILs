0d65c6694dab1f8875317282045a60dc03adb552
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-03-28 13:54:43 +0900

    Title: 이상한 폭탄 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240328/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define MAX_N 100
+using namespace std;
+int n,k;
+int arr[MAX_N];
+int main(){
+    // 입력
+cin>>n>>k;
+
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+int ans=-1;
+//모든 쌍에 대해서 터질 수 있는 폭탄을 찾고
+//그 중 번호가 최대인 값을 찾습니다.
+for(int i=0;i<n;i++)
+for(int j=i+1;j<n;j++){
+    if(j-i>k)
+    break;
+
+    if(num[i]!=num[j])
+    continue;
+
+    ans=max(ans,num[i]);
+}
+cout<<ans;
+
+return 0;
+}
