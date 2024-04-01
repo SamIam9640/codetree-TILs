99756edae30df9ce677dac141cf196ad595371eb
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-01 13:03:52 +0900

    Title: 정수 명령 처리 5, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240401/정수 명령 처리 5/process-numeric-commands-5.cpp
+++ 240401/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -1,37 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    vector<int> dynamicArray;
-    string command;
-    
-    while (getline(cin, command)) {
-        stringstream ss(command);
-        string operation;
-        ss >> operation;
-        
-        if (operation == "push_back") {
-            int num;
-            ss >> num;
-            dynamicArray.push_back(num);
-        } else if (operation == "pop_back") {
-            if (!dynamicArray.empty()) {
-                dynamicArray.pop_back();
-            } 
-        } else if (operation == "size") {
-            cout<< dynamicArray.size() << endl;
-        } else if (operation == "get") {
-            int k;
-            ss >> k;
-            if (k >= 1 && k <= dynamicArray.size()) {
-                cout<< dynamicArray[k - 1] << endl;
-            } 
-        } 
-    }
-    
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v;
+
+    for(int i=0;i<n;i++){
+
+    v.push_back (10);
+    v.push_back (20);
+    cout<<v[0]<<"\n";
+    cout<<v[1]<<"\n";
+    cout<<v.size()<<"\n";
+
+    v.pop_back();
+    cout<<v.size()<<"\n";
+
+    cout<<v[0]<<"\n";
+    cout<<v.size()<<"\n";
     return 0;
+
+    }
 }
