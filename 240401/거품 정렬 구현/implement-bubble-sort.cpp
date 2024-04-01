f5aae176ea8e6009c6edaed727c1ed294d49d45c
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-01 23:57:00 +0900

    Title: 거품 정렬 구현, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240401/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(){
+    cin>>n;
+
+function bubble_sort(qrr[])
+set len=n.size
+set sorted=true
+
+do{
+    sorted=true
+    for(i=0;i<len-1)
+    if(a[i]>a[i+1])
+    set tmp=a[i];
+    a[i]=a[i+1];
+    a[i+1]=tmp
+    sorted=false
+}
+while(sorted==false)
+
+return arr;
+
+}
