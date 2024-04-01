833842ffdaf1bdefcba02407dd21cc6d5eeef7c5
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-01 23:59:45 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240401/거품 정렬 구현/implement-bubble-sort.cpp
+++ 240401/거품 정렬 구현/implement-bubble-sort.cpp
@@ -1,35 +1,39 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <algorithm>
 
 #define MAX_N 100
 
+using namespace std;
+
+// 변수 선언
 int n;
 int arr[MAX_N];
 
 void BubbleSort() {
-    bool sorted=true;
+    bool sorted = true;
 
-do {
-    sorted=true;
-    for(int i=0;i<n-1;i++)
-    if(arr[i]>arr[i+1]){
-        int temp=arr[i];
-        arr[i+1]=temp;
-        sorted=false;
-    }
+    do {
+        sorted = true;
+        for(int i = 0; i < n - 1; i++)
+            if(arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                sorted = false;
+            }
+    } while(!sorted);
 }
-while(!sorted);
-}
 
-int main(){
-    cin>>n;
-    for(int i=0;i<n;i++)
-    cin>>arr[i];
+int main() {
+    // 입력
+    cin >> n;
 
-BubbleSort();
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    BubbleSort();
 
-for(int i=0;i<n;i++){
-    cout<<arr[i]<<" ";
-}
-return 0;
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
 }
