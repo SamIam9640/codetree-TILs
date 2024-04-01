c07d2cbd7db3cab47632226ded957ee4c7d062f9
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-01 12:56:31 +0900

    Title: 정수 명령 처리 5, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240401/정수 명령 처리 5/process-numeric-commands-5.cpp
+++ 240401/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -1,15 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int n;
-    cin>>n;
-    vector<int> v;
-
-    for(int i=0;i<n;i++){
-
-    v.push_back (10);
-    v.push_back (20);
 #include <iostream>
 #include <vector>
 #include <string>
