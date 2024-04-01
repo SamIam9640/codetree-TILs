d34f3c558d130bfba3689f82eafd6f192add4371
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-01 17:58:49 +0900

    Title: 정수 명령 처리 8, Time: 20ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240401/정수 명령 처리 8/process-numeric-commands-8.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+    cin>>n;
+    list <int> l;
+
+    for(int i=0;i<n;i++){
+    string command;
+    cin>>command;
+
+    if(command=="push_back"){
+        int num;
+        cin>>num;
+        l.push_back(num);
+    }
+    else if(command=="push_front"){
+        int n;
+        cin>>n;
+        l.push_front(n);
+    }
+    else if(command=="pop_front"){
+        int v=l.front();
+        l.pop_front();
+        cout<<v<<"\n";
+    }
+    else if(command=="pop_back"){
+        int v=l.back();
+        l.pop_back();
+        cout<<v<<"\n";
+    }
+    else if(command=="size"){
+        cout<<l.size()<<"\n";
+    }
+    else if(command=="empty"){
+        if(l.empty()) cout<<"1"<<"\n";
+        else cout<<"0"<<"\n";
+    }
+    else if(command=="front"){
+        cout<<l.front()<<"\n";
+    }
+    else {
+        cout<<l.back()<<"\n";
+    }
+    }
+
+    return 0;
+}
