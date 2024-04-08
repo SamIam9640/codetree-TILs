beee68af6cd84957fef6df6d01d6cefae34479f3
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-08 20:28:51 +0900

    Title: 최고의 33위치, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/최고의 33위치/best-place-of-33.cpp
+++ 240408/최고의 33위치/best-place-of-33.cpp
@@ -1,20 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-#define MAX_N 20
-int n;
-int arr[MAX_N];
 
-GetNumberOfCoin (){
-
-}
 int main() {
-cin>>n;
-for(int i=0;i<n;i++){
-    cin>>arr[i];
-}
+    int N;
+    cin >> N;
 
-GetNumberOfCoin(n);
+    vector<vector<int>> grid(N, vector<int>(N, 0));
 
-cout<<
+    // 격자 정보 입력
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cin >> grid[i][j];
+        }
+    }
 
+    int maxCoins = 0;
+
+    // 3*3 격자 탐색
+    for (int i = 0; i <= N - 3; ++i) {
+        for (int j = 0; j <= N - 3; ++j) {
+            int coins = 0;
+            // 3*3 격자 내의 동전 개수 계산
+            for (int row = i; row < i + 3; ++row) {
+                for (int col = j; col < j + 3; ++col) {
+                    coins += grid[row][col];
+                }
+            }
+            // 최대 동전 개수 갱신
+            maxCoins = max(maxCoins, coins);
+        }
+    }
+
+    cout << maxCoins << endl;
+
+    return 0;
 }
