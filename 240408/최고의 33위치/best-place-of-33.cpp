33a5c8e33e7afa2fa11629254b1ad71b39edf13b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-08 20:27:19 +0900

    Title: 최고의 33위치, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/최고의 33위치/best-place-of-33.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_N 20
+int n;
+int arr[MAX_N];
+
+GetNumberOfCoin (){
+
+}
+int main() {
+cin>>n;
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+
+GetNumberOfCoin(n);
+
+cout<<
+
+}
