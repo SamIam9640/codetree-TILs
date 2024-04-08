c64e877a27c60bc7ce6b5eb976c0ef5086ba5ad7
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-08 21:31:14 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/최고의 33위치/best-place-of-33.cpp
+++ 240408/최고의 33위치/best-place-of-33.cpp
@@ -1,38 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX_N 20
+
 int n;
 int grid[MAX_N][MAX_N];
 
+int GetNumOfGold(int row_s,int row_e,int col_s,int col_e){
+    int num_of_gold=0;
 
-int GetNumOfGold(int row_s,int col_s, int row_e,int col_e){
-int num_of_gold=0;
+    for(int i=row_s;i<=row_e;i++)
+    for(int j=col_s;j<=col_e;j++)
+    num_of_gold+=grid[i][j];
 
-for(int i=row_s;i<=row_e;i++)
-for(int j=col_s;j<=col_e;j++)
-num_of_gold+=grid[i][j];
-
-return num_of_gold;
+    return num_of_gold;
 }
-
 
 int main() {
     int max_gold=0;
-cin>>n;
-for(int row=0;row<n;row++)
-for(int col=0;col<n;col++)
-cin>>grid[row][col];
+    cin>>n;
 
-for(int row=0;row<n;row++)
-for(int col=0;col<n;col++){
+    for(int row=0;row<n;row++)
+    for(int col=0;col<n;col++)
+    cin>>grid[row][col];
+
+    for(int row=0;row<n;row++){
+    for(int col=0;col<n;col++){
 if(row+2>=n||col+2>=n) continue;
 
-int num_of_gold=GetNumOfGold(row,col,row+2,col+2);
-
-max_gold=max(max_gold,num_of_gold);
-
-
-}
-
-cout<<max_gold;
+int num_of_gold=GetNumOfGold(row,row+2,col,col+2);
+    max_gold=max(max_gold,num_of_gold);
+    
+    }
+    }
+    cout<<max_gold;
+    return 0;
 }
