0c1b91895d331effb2a66933d4509d01f6b3c131
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-08 21:52:33 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240408/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define MAX_N 100
+using namespace std;
+int n;
+int grid[MAX_N][MAX_N];
+
+int ddd() {
+    int num_of_dig=0;
+
+
+    return num_of_dig;
+}
+
+int main(){
+cin>>n;
+
+for(int row=0;row<n;row++)
+for(int col=0;col<n;col++)
+cin>>grid[row][col];
+
+for(int row=0;row<n;row++){
+for(int col=0;col<n;col++){
+int num_of_dig=ddd(){}
+
+
+
+}
+}
+cout<<num_of_dig;
+return 0;
+}
