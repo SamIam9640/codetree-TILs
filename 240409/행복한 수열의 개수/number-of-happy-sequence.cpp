a9114e326405be8ff56652ba31da0e63e1661f35
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-09 09:26:14 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240409/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240409/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
-#define MAX_N 100
 using namespace std;
+#define MAX_N 100
 
 int n,m;
 int grid[MAX_N][MAX_N];
 int seq[MAX_N];
 
-bool IsHappySequence() {
-    int consecutive_count=1,max_ccnt=1;
+bool IsHappyNumber(){
+    int consecutive_num=1,max_ccnt=1;
+
     for(int i=1;i<n;i++){
     if(seq[i-1]==seq[i])
-    consecutive_count++;
+    consecutive_num++;
     else
-    consecutive_count=1;
-
-    max_ccnt=max(max_ccnt,consecutive_count);
-    }
+    consecutive_num=1;
+}
+    max_ccnt=max(max_ccnt,consecutive_num);
     return max_ccnt>=m;
 }
 
@@ -27,22 +27,21 @@ int main(){
     cin>>grid[i][j];
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++)
-        seq[j]=grid[i][j];
+    for(int j=0;j<n;j++)
+    seq[j]=grid[i][j];
 
-        if(IsHappySequence()){
-            num_happy++;
-        }
+    if(IsHappyNumber())
+    num_happy++;
     }
 
     for(int j=0;j<n;j++){
         for(int i=0;i<n;i++)
         seq[i]=grid[i][j];
 
-        if(IsHappySequence())
+        if(IsHappyNumber())
         num_happy++;
     }
 
-    cout<< num_happy;
+    cout<<num_happy;
     return 0;
 }
