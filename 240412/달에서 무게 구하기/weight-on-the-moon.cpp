433efc7f6e4284845563ae21564c5378c59ed5bd
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 14:17:21 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout<<fixed,cout.precision(6);
+
+    int a=13;
+    double b=0.165;
+    cout<<a<<" * "<<b<<" = "<<a*b;
+    return 0;
+}
