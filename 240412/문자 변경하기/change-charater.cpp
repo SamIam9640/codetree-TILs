075a13eac2873016491ba8dd3e40aab8e4d74b12
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 14:32:18 +0900

    Title: 문자 변경하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/문자 변경하기/change-charater.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 a에 'C'를 넣기
+    char a = 'C';
+    
+    // 변수 a에 'T'를 다시 넣기
+    a = 'T';
+    
+    // 변수 a 출력
+    cout << a << endl;
+    
+    return 0;
+}
