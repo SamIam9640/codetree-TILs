243b17bf7b036c8baedb561654eea5749fcc225a
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 19:21:51 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a=3,b=5;
+    int temp;
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    cout<<a<<"\n";
+    cout<<b;
+    return 0;
+}
