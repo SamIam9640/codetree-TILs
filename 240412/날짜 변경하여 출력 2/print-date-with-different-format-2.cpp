aa8962380777668af6ada63a335a57c28a367665
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 21:35:27 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int mm,dd,yyyy;
+
+    cin>>mm;
+    cin.get();
+    cin>>dd;
+    cin.get();
+    cin>>yyyy;
+
+    cout<<yyyy<<"."<<mm<<"."<<dd;
+    return 0;
+}
