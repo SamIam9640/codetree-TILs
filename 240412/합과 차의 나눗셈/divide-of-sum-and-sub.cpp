8312b282507203ce44a30ad20c8f2a8e2cec702f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 22:28:46 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    cout<<fixed,cout.precision(2);
+
+    cout<<double (a+b)/(a-b);
+
+    return 0;
+}
