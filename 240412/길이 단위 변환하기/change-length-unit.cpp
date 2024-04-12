ada62d302a9ff524d26e4ee48d98ba2a41cda828
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 14:25:19 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout<<fixed,cout.precision(1);
+
+
+    double a=30.48;
+    double b=160934;
+
+    cout<<9.2<<"ft"<<" = "<<9.2*a<<"cm"<<"\n";
+    cout<<1.3<<"mi"<<" = "<<1.3*b<<"cm";
+    return 0;
+    
+}
