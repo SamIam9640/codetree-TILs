bcab01dfd18ca67175c52da2c801c09c0d988695
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 23:00:24 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    cout<<a+b+c<<"\n";
+    cout<<(a+b+c)/3<<"\n";
+    cout<<a+b+c-(a+b+c)/3;
+    return 0;
+}
