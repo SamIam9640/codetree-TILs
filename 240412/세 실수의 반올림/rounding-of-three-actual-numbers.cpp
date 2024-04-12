0a8e354d6ea39773d7ac558bee61c3153960b98f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-12 19:55:50 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240412/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a,b,c;
+
+    cin>>a>>b>>c;
+
+    cout<<fixed,cout.precision(3);
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<c;
+    return 0;
+}
