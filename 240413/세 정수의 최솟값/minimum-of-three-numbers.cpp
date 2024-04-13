563c142395daa0e2f575fa936f7ed34e01e39fbd
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 17:06:00 +0900

    Title: 세 정수의 최솟값, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240413/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 240413/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -6,8 +6,8 @@ int main() {
     cin>>a>>b>>c;
 
     if(a<=b&&a<=c) cout<<a;
-    if(b<=a&&b<=c) cout<<b;
-    if(c<=a&&c<=b) cout<<c;
+    else if(b<=a&&b<=c) cout<<b;
+    else cout<<c;
 
     return 0;
 }
