1b3a00948e7426ce94b7b0303cf9bae33cb64695
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 21:44:12 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240413/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(i<=n) {
+        cout<<"*"<<"\n";
+        i++;
+    }
+    return 0;
+}
