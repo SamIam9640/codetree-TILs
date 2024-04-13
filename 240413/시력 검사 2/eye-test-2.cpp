f70e6ec43afe7458fbb9eca5e5689cbbd2355588
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 11:54:02 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240413/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a>=1.0) cout<<"High";
+    else if(a>=0.5) cout<<"Middle";
+    else cout<<"Low";
+    return 0;
+}
