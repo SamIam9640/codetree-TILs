cb79e801a1c61df62137cb566b75431de239416c
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 16:58:52 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240413/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    int min;
+
+    cin>>a>>b>>c;
+
+    if(a==min) cout<<"1 ";
+    else cout<<"0 ";
+
+    if(a==b==c) cout<<1;
+    else cout<<0;
+    return 0;
+}
