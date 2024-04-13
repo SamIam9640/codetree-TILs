c3ef2a97a9334778f83bc4072413683041e9a68a
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 22:14:17 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240413/n번 더하기/add-n-times.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,n;
+    cin>>a>>n;
+
+    for(int i=0;i<n;i++){
+        a+=n;
+        cout<<a<<"\n";
+    }
+    return 0;
+}
