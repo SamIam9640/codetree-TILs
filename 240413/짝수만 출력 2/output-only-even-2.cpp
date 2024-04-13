e5e3cf6e094093d236bd4592e276f2cde77a6660
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 22:06:54 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240413/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int b,a;
+    cin>>b>>a;
+
+    int i=b;
+
+    while(i>=a){
+        cout<<i<<" ";
+        i-=2;
+
+    }
+    return 0;
+}
