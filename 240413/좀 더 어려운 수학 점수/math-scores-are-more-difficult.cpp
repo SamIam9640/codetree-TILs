264f536de632679d5074b9a488254528a4e15f4f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 17:26:35 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240413/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240413/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -1,15 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-int a1,b1,a2,b2;
+    int a1,a2,b1,b2;
 
-cin>>a1>>b1>>a2>>b2;
+    cin>>a1>>a2>>b1>>b2;
 
-if(a1>b1) cout<<"A";
-else if(a1<b1) cout<<"B";
-else if(a1==b1&&a1>b1) cout<<"A";
-else if(a1==b1&&a1<b1) cout<<"B";
+    if(a1>b1||(a1==b1)&&a2>b2) cout<<"A";
+    else cout<<"B";
 
-return 0;
-
+    return 0;
 }
