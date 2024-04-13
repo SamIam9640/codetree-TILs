919df84befe3ed45968ab6da48fc18331dea3d35
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 22:19:51 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240413/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+
+    if(a%1==0){
+        for(int i=1;i<=b;i++){
+            cout<<a;
+        }
+    }
+    return 0;
+}
