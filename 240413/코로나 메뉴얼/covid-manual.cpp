6acc30f90a3adc08520c893fa224d2e76fba0284
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-13 20:56:21 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240413/코로나 메뉴얼/covid-manual.cpp
+++ 240413/코로나 메뉴얼/covid-manual.cpp
@@ -3,23 +3,22 @@ using namespace std;
 
 int main() {
     char c1,c2,c3;
-    int t1,t2,t3;
+    int a1,a2,a3;
 
-    cin>>c1>>t1;
-    cin>>c2>>t2;
-    cin>>c3>>t3;
+    cin>>c1>>a1;
+    cin>>c2>>a2;
+    cin>>c3>>a3;
 
-    if(c1=='Y'&&t1>=37){
-        if((c2=='Y'&&t2>=37)||(c3=='Y'&&t3>=37))
+    if(c1=='Y'&&a1>=37){
+        if((c2=='Y'&&a2>=37)||(c3=='Y'&&a3>=37))
         cout<<"E";
-        else
-        cout<<"N";
+        else cout<<"N";
     }
     else{
-        if((c2=='Y'&&t2>=37)&&(c3=='Y'&&t3>=37))
+        if((c2=='Y'&&a2>=37)&&(c3=='Y'&&a3>=37))
         cout<<"E";
-        else
-        cout<<"N";
+        else cout<<"N";
     }
-    return 0;
+
+ return 0;   
 }
