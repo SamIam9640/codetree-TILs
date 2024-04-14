4283b508db11db1a8070c5824b4d57babd6d478e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-14 20:42:11 +0900

    Title: 조건에 부합하는 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240414/조건에 부합하는 수/number-that-meets-the-condition.cpp
+++ 240414/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -6,7 +6,11 @@ int main() {
     cin>>a;
 
     for(int i=1;i<=a;i++){
-       if(((i%2==0&&i%4!=0)||(i/8)%2==0)||((i%7)<4))
+       if(i%2==0&&i%4!=0)
+       continue;
+       if((i/8)%2==0)
+       continue;
+       if((i%7)<4)
        continue;
         else cout<<i<<" ";
     }
