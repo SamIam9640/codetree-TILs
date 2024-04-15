3de6fcf8be36a2a63111a862c0b79d10f96346b1
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-15 10:50:14 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240415/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   
+
+    while(1){
+         int num;
+    cin>>num;
+        if(num==1) {cout<<"John"<<"\n";}
+        else if(num==2) {cout<<"Tom"<<"\n";}
+        else if(num==3) {cout<<"Paul"<<"\n";}
+        else if(num==4) {cout<<"Sam"<<"\n";}
+        else {cout<<"Vacancy"<<"\n";
+        break;
+        }
+    }
+    return 0;
+}
