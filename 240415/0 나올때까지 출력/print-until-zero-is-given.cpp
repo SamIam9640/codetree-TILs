e782cc2b077c0b2992c2808d1b14bd1aae4c244a
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-15 10:33:06 +0900

    Title: 0 나올때까지 출력, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240415/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num;
+    while(true){
+        
+        cin>>num;
+        if(num==0) {break;}
+cout<<num<<"\n";
+    }
+    return 0;
+}
