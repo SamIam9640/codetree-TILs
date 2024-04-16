efe1a21bb4a6489c7d7ff919c6e2f861cf4c5653
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-17 00:37:54 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240417/별표 출력하기 8/print-star-8.cpp
+++ 240417/별표 출력하기 8/print-star-8.cpp
@@ -7,7 +7,7 @@ int main() {
 
     for(int i=0;i<n;i++){
         if(i%2==0) 
-        cout<<"* "<<"\n";
+        cout<<"* ";
         else 
         for(int j=0;j<i+1;j++){
             cout<<"* ";
