e32ec46407627177c5c67d764d8daa0c692f1e8e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-16 13:05:01 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240416/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    bool satisfied=true;
+
+    for(int i=a;i<=b;i++){
+        if(i%c==0) satisfied=false;
+    }
+    if(satisfied==false) cout<<"No";
+    else cout<<"YES";
+
+    return 0;
+
+    
+}
