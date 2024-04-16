5fa5699018827e25b2c756c9a3d46545273aebf5
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-04-16 23:32:18 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240416/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
+++ 240416/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -14,4 +14,11 @@ int main() {
         }
         cout<<"\n";
     }
+    for(int i=n-1;i>0;i--){
+        for(int j=0;j<i;j++){
+            cout<<"@ ";
+        }
+        cout<<"\n";
+    }
+return 0;
 }
