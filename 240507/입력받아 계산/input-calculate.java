470cdeace191f69243124c179eef5984907f9b19
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-07 16:25:42 +0900

    Title: 입력받아 계산, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240507/입력받아 계산/input-calculate.java
+++ 240507/입력받아 계산/input-calculate.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int a=sc.nextInt();
-        System.out.println(a);
+        System.out.println(a+2);
         // 여기에 코드를 작성해주세요.
     }
 }
