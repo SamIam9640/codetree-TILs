30be32ea06056db1cd3d46fb465cef1d75ad5e1f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-07 15:46:50 +0900

    Title: 변수 값 복사하기 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240507/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a=5,b=6,c=7;
+
+        a=b=c;
+
+        System.out.print(a+" ");
+        System.out.print(b+ " ");
+        System.out.print(c);
+    }
+}
