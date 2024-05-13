a2764a802f7dd3139ce0a3696b65a0f2ad30c2ed
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-13 20:24:13 +0900

    Title: 데이터 교환, Time: 105ms, Memory: 8MB, Status: Passed - CodeTree

--- 240513/데이터 교환/exchange-data.java
+++ 240513/데이터 교환/exchange-data.java
@@ -3,10 +3,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a=5,b=6,c=7;
 
-        int temp=a;
+        int temp=b;
+        b=a;
         a=c;
-        c=b;
-        b=temp;
+        c=temp;
 
         System.out.println(a);
         System.out.println(b);
