16a4ddc3dba5efdb40f03b3a3ab8fff90f1a695d
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-17 16:11:13 +0900

    Title: 3 또는 5의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240517/3 또는 5의 배수/multiples-of-3-or-5.java
+++ 240517/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -10,10 +10,10 @@ public class Main {
         int b=sc.nextInt();
 
         if(a%3==0){
-            System.out.print("YES");
+            System.out.println("YES");
         }
         else{
-            System.out.print("NO");
+            System.out.println("NO");
         }
         if(a%5==0){
             System.out.print("YES");
