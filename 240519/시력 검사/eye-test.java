5f72e10358b2025e9d80625ffe41763db0ec5a72
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-19 20:08:03 +0900

    Title: 시력 검사, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240519/시력 검사/eye-test.java
+++ 240519/시력 검사/eye-test.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
 
-        double r_eye=sc.nextDouble();
-        double l_eye=sc.nextDouble();
+        double a=nextDouble();
+        double b=nextDouble();
 
-        if(r_eye>=1.0&&l_eye>=1.0){
+        if(a>=1.0%%b>=1.0){
             System.out.print("High");
         }
-        else if(r_eye>=0.5&&l_eye>=0.5){
+        else if(a>=0.5&&b>=0.5){
             System.out.print("Middle");
         }
         else{
