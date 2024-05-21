a33d24a555ac490ddf334e7f0f697e09d092f859
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-21 20:58:41 +0900

    Title: 홀수만 출력하기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240521/홀수만 출력하기/print-only-odd-numbers.java
+++ 240521/홀수만 출력하기/print-only-odd-numbers.java
@@ -6,8 +6,10 @@ public class Main {
     Scanner sc=new Scanner(System.in);
 
     int n=sc.nextInt();
+
     for(int i=0;i<n;i++){
         int a=sc.nextInt();
+
         if(a%2!=0&&a%3==0)
         System.out.println(a);
     }
