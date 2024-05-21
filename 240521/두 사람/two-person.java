803ff8e8f73f37b5262929bf7470f691626508bc
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-21 14:12:59 +0900

    Title: 두 사람, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240521/두 사람/two-person.java
+++ 240521/두 사람/two-person.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner sc=new Scanner(System.in);
 
         int a_age=sc.nextInt();
-        int a_sex=sc.string[0].charat();
+        char a_sex=sc.next().charAt(0);
         int b_age=sc.nextInt();
-        int b_sex=sc.string[0].charAt();
+        char b_sex=sc.next().charAt(0);
 
-        if((a_age>=19&&a_sex=="M")||(b_age>=19&&b_sex=="M")){
+        if((a_age>=19&&a_sex=='M')||(b_age>=19&&b_sex=='M')){
             System.out.print(1);
         }
     }
