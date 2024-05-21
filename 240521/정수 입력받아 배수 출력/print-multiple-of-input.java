1715762fe90b33be55b593e108304034a19a6de1
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-21 19:06:27 +0900

    Title: 정수 입력받아 배수 출력, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240521/정수 입력받아 배수 출력/print-multiple-of-input.java
+++ 240521/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -8,7 +8,7 @@ public class Main {
     int n=sc.nextInt();
 
     for(int i=0;i<5;i++){
-        for(int i=n;i<100;i+=n){
+        for(int a=n;a<100;a+=n){
             System.out.print(i+" ");
         }
     }
