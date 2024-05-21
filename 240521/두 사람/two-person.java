24ebc0db47bcf0c85ff5888baab9103e04040ce8
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-21 14:20:00 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240521/두 사람/two-person.java
+++ 240521/두 사람/two-person.java
@@ -12,10 +12,10 @@ public class Main {
         char b_gen=sc.next().charAt(0);
 
         if((a_age>=19&&a_gen=='M')||(b_age>=19&&b_gen=='M')){
-            System.out.print(1);
+            System.out.print("1");
         }
         else{
-            System.out.print(0);
+            System.out.print("0");
         }
     }
 }
