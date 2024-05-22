e173ce4c7525fc9cf83357fb187d64f4654f24df
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 20:21:46 +0900

    Title: 3n + 1 수열, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240522/3n + 1 수열/3n-plus-1-sequence.java
+++ 240522/3n + 1 수열/3n-plus-1-sequence.java
@@ -4,9 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int cnt=0;
+               int n=sc.nextInt();
         // 여기에 코드를 작성해주세요.
     while(true){
-        int n=sc.nextInt();
+ 
 
         if(n==1)
         break;
