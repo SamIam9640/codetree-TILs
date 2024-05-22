312d9ab32c6c4a5963ebe5482c8b1de0f793b254
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 15:52:50 +0900

    Title: 1부터의 합, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

--- 240522/1부터의 합/sum-from-the-one.java
+++ 240522/1부터의 합/sum-from-the-one.java
@@ -11,9 +11,10 @@ public class Main {
     for(int i=1;i<=100;i++){
         sum+=i;
         if(sum>=n){
-        System.out.print(i);
+       
+    System.out.print(i);
         break;}
     }
-   
+  
     }
 }
