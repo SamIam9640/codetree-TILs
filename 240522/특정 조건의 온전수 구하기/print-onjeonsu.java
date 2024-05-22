570484ca44eff153cd86bdd9614777896bc1cf48
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 14:47:37 +0900

    Title: 특정 조건의 온전수 구하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240522/특정 조건의 온전수 구하기/print-onjeonsu.java
+++ 240522/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -9,8 +9,9 @@ public class Main {
 
     for(int i=1;i<=n;i++){
         if(i%2!=0&&i%10!=5&&(i%3==0&&i%9!=0))
-        continue;
+       continue;
+       System.out.print(i+" ");
     }
-    System.out.print(i+" ");
+  
     }
 }
