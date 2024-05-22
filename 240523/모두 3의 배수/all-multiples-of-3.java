295e857b5da4bc0fcbafae8490315058e00bfcf9
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-23 03:03:36 +0900

    Title: 모두 3의 배수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240523/모두 3의 배수/all-multiples-of-3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    boolean satisfied=true;
+    for(int i=0;i<5;i++){
+        int n=sc.nextInt();
+
+        if(n%3!=0)
+        satisfied=false;
+    }
+    if(satisfied==true)
+    System.out.print(1);
+    else
+    System.out.print(0);
+    }
+}
