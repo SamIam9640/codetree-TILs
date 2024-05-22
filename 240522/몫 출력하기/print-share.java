c0d14384756cc00e7cfdbd82a6f911d431587a37
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 18:18:10 +0900

    Title: 몫 출력하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 240522/몫 출력하기/print-share.java
+++ 240522/몫 출력하기/print-share.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
+        int cnt=0;
         
         Scanner sc=new Scanner(System.in);
     while(true){
        
         int n=sc.nextInt();
-        int cnt=0;
+        
 
         if(n%2!=0)
         continue;
