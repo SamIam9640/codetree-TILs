744e53a7d791fbcf989deab476cb23f68df8d533
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 16:56:50 +0900

    Title: 1까지 나누기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240522/1까지 나누기/divide-by-1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    Scanner sc=sc.nextInt();
+
+    int cnt=0;
+    int n=sc.nextInt();
+
+    for(int i=1;i<=100;i++){
+        n/=i;
+        cnt++;
+        if(n<=1){
+            System.out.print(cnt);
+            break;
+        }
+    }
+    
+    }
+}
