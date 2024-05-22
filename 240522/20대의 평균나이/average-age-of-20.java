316d88c9eb2dccded1cd1942f2b73ca7932ec439
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 20:00:17 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240522/20대의 평균나이/average-age-of-20.java
+++ 240522/20대의 평균나이/average-age-of-20.java
@@ -3,22 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc=new Scanner(System.in);
-    int sum=0;
-    int cnt=0;
-   
+    Scanner sc=new Scanner(System.in);
     while(true){
-        int n=sc.nextInt();
+    int n=sc.nextInt();
+    int cnt=0;
+    int sum=0;
+    double age;
 
+    sum+=n;
+    cnt++;
 
-        
-        if(n!=20){
-            System.out.printf("%.2f",(double) sum/cnt);
-            break;
-        }
-        
-        cnt++;
-        sum+=n;
+    if(n>19&&n<30){
+        System.out.printf("%.2f",(doube) sum/cnt);
+        break;
+    }
+    
     }
     }
 }
