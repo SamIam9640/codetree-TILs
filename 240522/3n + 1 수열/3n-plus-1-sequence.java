8e1a649eb34ebe8418c9ddfd01f48e0004b2cc48
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 20:27:02 +0900

    Title: 3n + 1 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240522/3n + 1 수열/3n-plus-1-sequence.java
+++ 240522/3n + 1 수열/3n-plus-1-sequence.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int cnt=0;
-               int n=sc.nextInt();
         // 여기에 코드를 작성해주세요.
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
     while(true){
- 
-
         if(n==1)
         break;
 
         if(n%2!=0)
-        n=3*n+1;
-        else
         n/=2;
+        else
+        n=3*n+1;
         cnt++;
+
+
     }
-System.out.println(cnt);
+    System.out.print(cnt);
     }
 }
