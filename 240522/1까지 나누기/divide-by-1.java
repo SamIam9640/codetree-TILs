fe8e1c066bf28297ab948efa46d9c4df5e3b3e0f
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 16:57:14 +0900

    Title: 1까지 나누기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240522/1까지 나누기/divide-by-1.java
+++ 240522/1까지 나누기/divide-by-1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-    Scanner sc=sc.nextInt();
+    Scanner sc=new Scanner(System.in);
 
     int cnt=0;
     int n=sc.nextInt();
