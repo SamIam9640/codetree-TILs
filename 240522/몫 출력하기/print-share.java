53614ac94eca2ca0b7a33573dd85b3fa4eb7158e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-22 18:18:46 +0900

    Title: 몫 출력하기, Time: 195ms, Memory: 10MB, Status: Passed - CodeTree

--- 240522/몫 출력하기/print-share.java
+++ 240522/몫 출력하기/print-share.java
@@ -13,10 +13,10 @@ public class Main {
 
         if(n%2!=0)
         continue;
-        
+        else{
             System.out.println(n/2);
         cnt++;
-        
+        }
         if(cnt>=3)
         break;
          }
