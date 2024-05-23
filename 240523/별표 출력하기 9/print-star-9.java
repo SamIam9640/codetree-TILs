1c09aa0cd3845142a65f2f7a0640317eb47d24f0
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-23 16:05:05 +0900

    Title: 별표 출력하기 9, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240523/별표 출력하기 9/print-star-9.java
+++ 240523/별표 출력하기 9/print-star-9.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-    
+    Scanner sc=new Scanner(System.in);
     //변수 선언 및 입력
     int n=sc.nextInt();
 
