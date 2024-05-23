76e7421643e92a54bb76863b66abd41f82630c57
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-23 13:40:26 +0900

    Title: 건너뛰고 별표 출력하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240523/건너뛰고 별표 출력하기/skip-and-print-a-star.java
+++ 240523/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -9,13 +9,15 @@ public class Main {
         for(int j=0;j<=i;j++)
         System.out.print("*");
         System.out.println();
+        System.out.println();
     }
-    for(int i=n-1;i>0;i--){
-        for(int j=0;j<=i;j++){
-            System.out.print("*");
-            System.out.println();
-        }
+    for(int i=n-2;i>=0;i--){
+        for(int j=0;j<=i;j++)
+        System.out.print("*");
+        System.out.println();
+        System.out.println();
     }
+ 
     
     }
 }
