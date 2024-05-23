557cc0677cf60961e7be940277806e9a1d237934
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-23 13:27:00 +0900

    Title: 별표 출력하기 4, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240523/별표 출력하기 4/print-star-4.java
+++ 240523/별표 출력하기 4/print-star-4.java
@@ -6,18 +6,15 @@ public class Main {
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
 
-    for(int i=0;i<n;i++){
-        for(int j=n-i-1;j>=0;j--){
-            System.out.print("* ");
-        }
+    for(int i=n-i;i>0;i--){
+        for(int j=0;j<=i;j++)
+        System.out.print("* ");       
         System.out.println();
     }
-    for(int i=n-2;i>=0;i--){
-        for(int j=n-i-1;j>=0;j--){
-            System.out.print("* ");
-        }
+    for(int i=n-1-i;i>0;i--){
+        for(int j=0;j<=i;j++)
+        System.out.print("* ");       
         System.out.println();
-    }
     
     }
 }
