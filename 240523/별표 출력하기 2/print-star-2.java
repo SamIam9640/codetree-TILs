4cec170fbfe2f3abdb501b59bdbd67c91d611ad1
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-23 10:06:02 +0900

    Title: 별표 출력하기 2, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240523/별표 출력하기 2/print-star-2.java
+++ 240523/별표 출력하기 2/print-star-2.java
@@ -8,7 +8,7 @@ public class Main {
     int n=sc.nextInt();
 
     for(int i=0;i<n;i++){
-        for(int j=0;j<=n-i;j++){
+        for(int j=0;j<n-i;j++){
             System.out.print("* ");
         }
         System.out.println();
