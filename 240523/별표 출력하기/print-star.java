fb798211026a09bcac17d4839341607f2e33a231
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-23 12:51:56 +0900

    Title: 별표 출력하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240523/별표 출력하기/print-star.java
+++ 240523/별표 출력하기/print-star.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-    int n=sc.nextInt();
+    
     Scanner sc=new Scanner(System.in);
-
-    for(int i=0;i<=n;i++){
+int n=sc.nextInt();
+    for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++)
             System.out.print("* ");
             System.out.println();
     }
-    for(int i=n-1;i>=1;i--){
+    for(int i=n-2;i>=0;i--){
         for(int j=0;j<=i;j++)
         System.out.print("* ");
         System.out.println();
