470cb414c438fbd305bdeb91659138c1599eb81b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-24 14:20:22 +0900

    Title: 체크 다이아몬드, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240524/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    
+    Scanner sc=new Scanner(System.in);
+    //변수 선언 및 입력
+    int n=sc.nextInt();
+    
+
+    //모양에 맞춰 별 출력
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=0;j++){
+            System.out.print(" ");
+        }
+        for(int j=0;j<n;j++){
+            System.out.print("* ");
+        }
+    System.out.println();
+
+    //모양에 맞춰 아래별 출력
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<i+1;j++){
+            System.out.print(" ");
+        }
+        for(int j=n-1;j>=0;j--){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    }
+    }
+}
