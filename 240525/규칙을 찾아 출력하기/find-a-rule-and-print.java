10572e389070b1b8011b87ce15078bfe94e1e37a
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-25 19:19:36 +0900

    Title: 규칙을 찾아 출력하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240525/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240525/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -12,8 +12,12 @@ public class Main {
         for(int j=0;j<n;j++){
             if(i==0||i==n-1||j==0||j==n-1)
             System.out.print("* ");
+            else
+            System.out.print("  ");
             if(i>j)
             System.out.print("* ");
+            else
+            System.out.print("  ");
         }
     }
     
