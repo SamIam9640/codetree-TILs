646cc7ad0c85f905a67741ac90b94559288ca703
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-25 17:29:00 +0900

    Title: 별표 출력하기 13, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240525/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    
+    Scanner sc=new Scanner(System.in);
+    //변수 선언 및 입력
+    int n=sc.nextInt();
+    
+    for(int i=0;i<2*n;i++){
+        if(i%2==0){
+            for(int j=0;j<n-i/2;j++)
+            System.out.print("* ");
+            System.out.println();
+        }
+        else{
+            for(int j=0;j<1+(i-1)/2;j++)
+            System.out.print("* ");
+            System.out.println();
+        }
+    }
+    }
+}
