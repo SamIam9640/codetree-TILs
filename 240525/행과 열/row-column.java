30a7d4fb3a15a82a4a4cad410ef24caae1c9443a
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-25 23:22:24 +0900

    Title: 행과 열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240525/행과 열/row-column.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //변수 선언 및 입력
+        int n=sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        //칸에 알맞은 숫자 입력하기
+        for(int i=1;i<=a;i++){
+            for(int i=1;i<=b;i++){
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
