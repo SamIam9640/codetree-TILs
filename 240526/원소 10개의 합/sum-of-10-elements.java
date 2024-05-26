8693bd70036460fec539dbbe33a5200f3d35b767
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-26 20:06:00 +0900

    Title: 원소 10개의 합, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240526/원소 10개의 합/sum-of-10-elements.java
+++ 240526/원소 10개의 합/sum-of-10-elements.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int sum=0;
+    int []arr=new int[10];
     for(int i=0;i<10;i++){
-        int n=sc.nextInt();
-        sum+=n;
+        arr[i]=sc.nextInt();
+        sum+=arr[i];
     }
     System.out.print(sum);
             // 여기에 코드를 작성해주세요.
