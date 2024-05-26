45e574f168c3a151d78b333fc799a9c2695ec831
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-26 18:57:47 +0900

    Title: 숫자 피라미드, Time: 209ms, Memory: 11MB, Status: Passed - CodeTree

--- 240526/숫자 피라미드/number-pyramid.java
+++ 240526/숫자 피라미드/number-pyramid.java
@@ -8,7 +8,7 @@ public class Main {
 
     for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++){
-            System.out.print(i+1);
+            System.out.print(i+1+" ");
         }
         System.out.println();
     }
