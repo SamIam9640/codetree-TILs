5ff24166a685e800afa2961aa0e01f4f3f9377c7
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-26 18:33:50 +0900

    Title: 정상적인 구구단, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240526/정상적인 구구단/normal-multiple.java
+++ 240526/정상적인 구구단/normal-multiple.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.int);
+       Scanner sc=new Scanner(System.in);
         //변수 선언 및 입력
         int n=sc.nextInt();
 
