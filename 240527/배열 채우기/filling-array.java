254d622193728e53b9271e645304a74779610595
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-27 16:05:29 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240527/배열 채우기/filling-array.java
+++ 240527/배열 채우기/filling-array.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
 
-    int []arr=new arr[10];
+    int []arr=new int[10];
     
     for(int i=0;i<10;i++){
         arr[i]=sc.nextInt();
