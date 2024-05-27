e039d690771e09b6920858173303fedbc4f2656d
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-27 16:14:55 +0900

    Title: 10개 입력받기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240527/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+   
+    int sum=0;
+    int cnt=0;
+    double avg;
+    int []arr=new int[10];
+    
+    for(int i=0;i<10;i++){
+        arr[i]=sc.nextInt();
+        if(arr[i]==0)
+        break;
+        sum+=arr[i];
+        cnt++;
+    }
+  
+    avg=(double)sum/cnt;
+
+    System.out.printf("%d %.1f",sum,avg);
+
+    }
+}
