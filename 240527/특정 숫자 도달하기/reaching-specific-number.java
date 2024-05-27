980847a9ce80df904cb0f3e86ccd728f16071db7
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-27 10:44:00 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240527/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int sum=0;
+    int cnt=0;
+    double avg;
+    int [] arr=new int[10];
+    for(int i=0;i<10;i++)
+    arr[i]=sc.nextInt();
+    for(int i=0;i<10;i++){
+        
+        if(arr[i]>=250)
+            break;
+            sum +=arr[i];
+            cnt++;
+        
+    }
+    avg=(double)sum/cnt;
+    System.out.printf("%d %.1f",sum,cnt);
+        // 여기에 코드를 작성해주세요.
+    }
+}
