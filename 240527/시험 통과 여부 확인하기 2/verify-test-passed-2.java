040988923baa54bb189ddb2d64ee3da2979aa988
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-27 16:34:42 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/시험 통과 여부 확인하기 2/verify-test-passed-2.java
+++ 240527/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -1,38 +1,34 @@
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    int cnt=0;
-    int sum=0;
-    double avg;
-    int n=sc.nextInt();
+        int cnt = 0;
+        int n = sc.nextInt();
 
-    for(int i=0;i<n;i++){
-        int []arr=new int[4];
-    
-    for(int j=0;j<4;j++){
-        arr[j]=sc.nextInt();
-    }
-    for(int j=0;j<4;j++){
-        
-    sum+=arr[j];
-    }
-    }
-    avg=(double)sum/4;
+        for(int i = 0; i < n; i++) {
+            int sum = 0;
+            int[] arr = new int[4];
+            
+            for(int j = 0; j < 4; j++) {
+                arr[j] = sc.nextInt();
+            }
+            
+            for(int j = 0; j < 4; j++) {
+                sum += arr[j];
+            }
 
-    if((avg%4)>=60){
-    System.out.println("pass");
-    cnt++;}
-    else
-    System.out.println("fail");
+            double avg = (double)sum / 4;
 
-    System.out.print(cnt);
+            if(avg >= 60) {
+                System.out.println("pass");
+                cnt++;
+            } else {
+                System.out.println("fail");
+            }
+        }
 
-
-
-        // 여기에 코드를 작성해주세요.
+        System.out.println(cnt);
     }
 }
