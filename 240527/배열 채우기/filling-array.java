6e9f3465b66fa21902a22900db8967e47b3f302a
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-27 16:06:02 +0900

    Title: 배열 채우기, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 240527/배열 채우기/filling-array.java
+++ 240527/배열 채우기/filling-array.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-
+    int cnt=0;
     int []arr=new int[10];
     
     for(int i=0;i<10;i++){
@@ -12,7 +12,7 @@ public class Main {
         break;
         cnt++;
     }
-    for(int i=cnt-1;i>0;i--){
+    for(int i=cnt-1;i>=0;i--){
        System.out.print(arr[i]+" ");
     }
 
