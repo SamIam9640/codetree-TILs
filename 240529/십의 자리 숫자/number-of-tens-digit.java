a8d25725d6cc451aef9b6ad301ab186a9bdcdd10
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-05-29 14:29:56 +0900

    Title: 십의 자리 숫자, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240529/십의 자리 숫자/number-of-tens-digit.java
+++ 240529/십의 자리 숫자/number-of-tens-digit.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
     int[] arr=new int [99];
     int[] countArr=new int[10];
+    Scanner sc=new Scanner(System.in);
 
     for(int i=0;i<99;i++){
         arr[i]=sc.nextInt();
