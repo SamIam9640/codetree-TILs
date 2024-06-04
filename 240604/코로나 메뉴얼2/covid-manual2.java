3b38fefd8227ba19e58563690ae27a2edf1a2f68
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-04 20:56:49 +0900

    Title: 코로나 메뉴얼2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240604/코로나 메뉴얼2/covid-manual2.java
+++ 240604/코로나 메뉴얼2/covid-manual2.java
@@ -2,37 +2,33 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
     
     int[] arr=new int[11];
     int[] newArr=new int[5];
+    int ans=0;
+
     char c;
-    int typeNum=0;
-
-    Scanner sc=new Scanner(System.in);
-
- 
-    for(int i=0;i<3;i++){
-          c=sc.next().charAt(0);
+    for(int i=0;i<4;i++){
+        c=sc.next().charAt(0);
         arr[i]=sc.nextInt();
-        if(c=='Y'&&arr[i]>=37)
-    typeNum=1;
-        if(c=='N'&&arr[i]>=37)
-        typeNum=2;
-        if(c=='Y'&arr[i]<37)
-        typeNum=3;
-        if(c=='N'&&arr[i]<37)
-        typeNum=4;
-        newArr[typeNum]++;
-    }
 
+        if(c=='Y'&&arr[i]>=37)
+        ans=1;
+        else if(c=='N'&&arr[i]>=37)
+        ans=2;
+        else if(c=='Y'&&arr[i]<37)
+        ans=3;
+        else if(c=='N'&&arr[i]<37)
+        ans=4;
+        newArr[ans]++;
+    }
     for(int i=1;i<=4;i++){
         System.out.print(newArr[i]+" ");
     }
     if(newArr[1]>=2)
     System.out.print("E");
-
     
-
     
     }
 }
