7aa12072950d2ea0aca6bfa2d4d38371e11a3ace
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-04 13:28:22 +0900

    Title: 1-9 개수 세기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    //변수 선언 및 입력
+    int n=sc.nextInt();
+
+    int[] arr=new int[100];
+    int[] newArr=new int[10];
+
+    for(int i=0;i<n;i++)
+    arr[i]=sc.nextInt();
+
+    for(int i=0;i<n;i++)
+    newArr[arr[i]]++;
+
+    for(int i=1;i<10;i++)
+    System.out.println(newArr[i]);
+    }
+}
