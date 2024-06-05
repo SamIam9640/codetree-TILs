b3190a0653825333ef40566b364702af5123392e
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-05 12:31:35 +0900

    Title: 배열 놀이, Time: 161ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240605/배열 놀이/play-with-array.java
+++ 240605/배열 놀이/play-with-array.java
@@ -21,13 +21,14 @@ public class Main {
             System.out.println(arr[a-1]);
         }
         else if(qtype==2){
-            int b=sc.nextInt();
+            int a=sc.nextInt();
             int idx=-1;
             for(int j=0;j<n;j++){
             
-            if(arr[i]==b)
-            idx=i;
+            if(arr[j]==a){
+            idx=j;
             break;
+            }
             }
             System.out.println(idx+1);
         }
@@ -36,7 +37,7 @@ public class Main {
             int b=sc.nextInt();
 
             for(int j=a-1;j<b;j++)
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[j]+" ");
             
         }
     }
