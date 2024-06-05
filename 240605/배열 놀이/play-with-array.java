acf41b105487fc6cd8790b3aab2fe95b1ceb0c6b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-05 13:12:40 +0900

    Title: 배열 놀이, Time: 167ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240605/배열 놀이/play-with-array.java
+++ 240605/배열 놀이/play-with-array.java
@@ -2,42 +2,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    int n=sc.nextInt();
-    int q=sc.nextInt();    
-    int[] arr=new int[100];
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[100];
 
-    for(int i=0;i<n;i++){
-        arr[i]=sc.nextInt();
-    }
-
-    for(int i=0;i<q;i++){
-       int qtype=sc.nextInt();
-       if(qtype==1){
-        int a=sc.nextInt();
-
-        System.out.println(arr[a-1]);
-       }
-       else if(qtype==2){
-        int a=sc.nextInt();
-        int idx=-1;
-
-        for(int j=0;j<n;j++){
-            if(arr[j]==a){
-                idx=j;
-                break;
-            }
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
-        System.out.println(idx+1);
-       }
-       else {
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        for(int j=a-1;j<b;j++)
-        System.out.print(arr[j]+" ");
-       }
-        
-    }
+
+        for(int i = 0; i < q; i++){
+            int qtype = sc.nextInt();
+            if(qtype == 1){
+                int a = sc.nextInt();
+                System.out.println(arr[a - 1]);
+            }
+            else if(qtype == 2){
+                int b = sc.nextInt();
+                int idx = -1;
+
+                for(int j = 0; j < n; j++){
+                    if(arr[j] == b){
+                        idx = j;
+                        break;
+                    }
+                }
+                System.out.println(idx + 1);
+            }
+            else {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                for(int j = a - 1; j < b; j++){
+                    System.out.print(arr[j] + " ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
