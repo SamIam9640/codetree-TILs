227c6d477d249fbb5337eb26d1afaa7a4fb3291b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-05 11:35:56 +0900

    Title: 개수 세기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240605/개수 세기/count-numbers.java
+++ 240605/개수 세기/count-numbers.java
@@ -10,7 +10,7 @@ public class Main {
     int[] arr=new int [100];
     int cnt=0;
 
-    for(int i=0;i<100;i++){
+    for(int i=0;i<n;i++){
         arr[i]=sc.nextInt();
         if(arr[i]==m)
         cnt++;
