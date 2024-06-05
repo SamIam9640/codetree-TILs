cdd2e8ed9f61319475c29d27cf5b5db58ff75660
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-05 13:57:02 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240605/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
+++ 240605/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -5,24 +5,21 @@ public class Main {
     Scanner sc=new Scanner(System.in);
 
     int n=sc.nextInt();
-    int cnt=0;
-    int position=0;
     int[] arr=new int[100];
+    int cnt=0;
 
     for(int i=0;i<n;i++){
         arr[i]=sc.nextInt();
     }
 
     for(int i=0;i<n;i++){
-        if(arr[i]==2){
+        if(arr[i]==2)
         cnt++;
         if(cnt==3){
-        position=i+1;
-        break;
-        }
+            System.out.print(i+1);
+            break;
         }
     }
-    System.out.print(position);
 
     }
 }
