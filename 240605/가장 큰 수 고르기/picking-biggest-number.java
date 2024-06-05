cbb0824325d2c10edd595b44887df136af0eb138
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-05 20:21:03 +0900

    Title: 가장 큰 수 고르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240605/가장 큰 수 고르기/picking-biggest-number.java
+++ 240605/가장 큰 수 고르기/picking-biggest-number.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        int [] arr=new int[10];
 
-        int[] arr=new int[10];
+        for(int i=0;i<10;i++)
+        arr[i]=sc.nextInt();
 
-        for(int i=0;i<10;i++){
-            arr[i]=sc.nextInt();
-        }
         int max_val=arr[0];
-        for(int i=0;i<10;i++){
-            if(arr[i]>max_val)
+
+        for(int i=0;i<10;i++)
+        if(max_val>arr[i]){
             max_val=arr[i];
         }
         System.out.print(max_val);
