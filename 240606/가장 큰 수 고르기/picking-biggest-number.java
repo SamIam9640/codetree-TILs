3d76a0551b11328b51a346e00aad2568c8d6be2b
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-06 09:40:59 +0900

    Title: 가장 큰 수 고르기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240606/가장 큰 수 고르기/picking-biggest-number.java
+++ 240606/가장 큰 수 고르기/picking-biggest-number.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int [] arr=new int[10];
+    Scanner sc=new Scanner(System.in);
 
-        for(int i=0;i<10;i++)
+    int[] arr=new int[10];
+
+    for(int i=0;i<10;i++){
         arr[i]=sc.nextInt();
+    }
 
-        int max_val=arr[0];
+    int maxval=arr[0];
 
-        for(int i=1;i<10;i++)
-        if(arr[i]>max_val){
-            max_val=arr[i];
-        }
-        System.out.print(max_val);
+    for(int i=1;i<10;i++){
+        if(arr[i]>maxval)
+        maxval=arr[i];
+    }
+    System.out.print(maxval);
+    
     }
 }
