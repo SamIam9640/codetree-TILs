92cae8f3a662c091d7c95b8a6ff6a6fc80e3d7e0
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-06 09:21:39 +0900

    Title: 999 또는 -999, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240606/999 또는 -999/999-or-999.java
+++ 240606/999 또는 -999/999-or-999.java
@@ -7,6 +7,7 @@ public class Main {
     int[]arr=new int[100];
     int maxval=arr[0];
     int minval=arr[0];
+
     for(int i=0;i<100;i++){
         arr[i]=sc.nextInt();
         if(arr[i]==999||arr[i]==-999)
@@ -14,8 +15,8 @@ public class Main {
         if(arr[i]>maxval){
             maxval=arr[i];
         }
-        if(minval>arr[i])
-        minval=arr[i];
+        if(minval>arr[i]){
+        minval=arr[i];}
     }
     System.out.print(maxval+" "+minval);
     }
