b35b50f677950d74fc61c7b23a1dcf42742d5945
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-07 09:32:38 +0900

    Title: 500 근처의 수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240607/500 근처의 수/near-500.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    int[] arr=new int[10];
+
+    for(int i=0;i<10;i++){
+    arr[i]=sc.nextInt();
+    }
+
+    int max=1;
+    int min=1000;
+
+    for(int i=0;i<10;i++){
+        if(arr[i]<500&&arr[i]>max)
+        max=arr[i];
+
+        else if(arr[i]>500&&min>arr[i])
+        min=arr[i];
+
+    }
+System.out.print(max+" "+min);
+    }
+}
