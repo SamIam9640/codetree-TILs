698a0459e937f765365a2ab91e146481a453bc94
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-10 14:28:20 +0900

    Title: 숫자 직사각형, Time: 213ms, Memory: 12MB, Status: Passed - CodeTree

--- 240610/숫자 직사각형/rectangle-of-numbers.java
+++ 240610/숫자 직사각형/rectangle-of-numbers.java
@@ -17,11 +17,11 @@ public class Main {
         }
     }
 
-    for(int i=0;i<n;i++)[
+    for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             System.out.print(arr2d[i][j]+ " ");
         }
         System.out.println();
-    ]
+    }
     }
 }
