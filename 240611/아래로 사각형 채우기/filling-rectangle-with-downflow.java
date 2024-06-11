81da6ade3946cec706833f418a291cd5a3cd9a26
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-11 14:12:09 +0900

    Title: 아래로 사각형 채우기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240611/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    int n=sc.nextInt();
+    int[][] arr2d=new int[10][10];
+    int num=1;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            arr2d[j][i]=num++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            System.out.print(arr2d[i][j]+" ");
+        }
+        System.out.println();
+    }
+    }
+}
