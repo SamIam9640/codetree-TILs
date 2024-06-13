28add437ba9bb95d9dcfc9b8a04c7a038adaeb7d
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-13 17:38:04 +0900

    Title: 가장 짧은 문자열, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240613/가장 짧은 문자열/shortest-string.java
+++ 240613/가장 짧은 문자열/shortest-string.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
@@ -13,15 +12,10 @@ public class Main {
     int len2=str2.length();
     int len3=str3.length();
 
-    int maxLength = Math.max(len1, Math.max(len2, len3));
+    int maxlen=Math.max(len1,Math.max(len2,len3));
+    int minlen=Math.min(len1,Math.min(len2,len3));
 
-        // 가장 짧은 문자열의 길이를 구합니다.
-        int minLength = Math.min(len1, Math.min(len2, len3));
+    System.out.print(maxlen-minlen);
 
-        // 길이 차이를 계산합니다.
-        int lengthDifference = maxLength - minLength;
-
-        // 결과를 출력합니다.
-        System.out.println(lengthDifference);
     }
 }
