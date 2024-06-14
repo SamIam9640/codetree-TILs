47d96ba1bbef989aaa3144037a73e01566aad4ee
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-14 11:07:57 +0900

    Title: 공백을 기준으로 출력, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240614/공백을 기준으로 출력/output-based-on-space.java
+++ 240614/공백을 기준으로 출력/output-based-on-space.java
@@ -2,32 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    String str1=sc.nextLine();
-    String str2=sc.nextLine();
-    char[] arr=new char[200];
-    int cnt=0;
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        char[] arr = new char[200]; // 입력받을 문자열의 총 길이가 200자를 넘지 않는다고 가정
 
-    int len1=str1.length();
-    int len2=str2.length();
+        int cnt = 0;
 
-    for(int i=0;i<len1;i++){
-        if(str1.charAt(i)!=' '){
-            arr[i]=str1.charAt(i);
-            cnt++;
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        // 첫 번째 문자열에서 공백을 제외한 문자만 배열에 저장
+        for (int i = 0; i < len1; i++) {
+            if (str1.charAt(i) != ' ') {
+                arr[cnt] = str1.charAt(i);
+                cnt++;
+            }
         }
-    }
-    for(int i=0;i<len2;i++){
-        if(str2.charAt(i)!=' '){
-            arr[cnt+1]=str2.charAt(i);
-            cnt++;
+
+        // 두 번째 문자열에서 공백을 제외한 문자만 배열에 저장
+        for (int i = 0; i < len2; i++) {
+            if (str2.charAt(i) != ' ') {
+                arr[cnt] = str2.charAt(i);
+                cnt++;
+            }
         }
-    }
-    for(int i=0;i<=cnt;i++){
-        System.out.print(arr[i]);
-    }
 
+        // 결과 출력
+        for (int i = 0; i < cnt; i++) {
+            System.out.print(arr[i]);
+        }
 
+        sc.close();
     }
 }
