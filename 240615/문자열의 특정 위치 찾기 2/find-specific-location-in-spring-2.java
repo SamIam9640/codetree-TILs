7adc545a36db679f226d63b0e7ebd03e6f3b0fe8
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-15 12:22:16 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 109ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240615/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.java
+++ 240615/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.java
@@ -10,13 +10,11 @@ public class Main {
     int cnt=0;
 
     for(int i=0;i<5;i++){
-        if(str[i].charAt(2)==c||str[i].charAt(3)==c){
+        if(str[i].charAt(2)==c||str[i].charAt(3)==c)
         System.out.println(str[i]);
         cnt++;
-        }
     }
 
     System.out.print(cnt);
-
     }
 }
