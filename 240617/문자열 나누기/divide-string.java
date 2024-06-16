d17d267601378ba18fba96cb87f500e17b4c64a7
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-17 01:14:38 +0900

    Title: 문자열 나누기, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240617/문자열 나누기/divide-string.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    String [] str=new String[10];
+    String totStr="";
+    
+    int n=sc.nextInt();
+
+    for(int i=0;i<n;i++){
+        str[i]=sc.next();
+    }
+
+    for(int i=0;i<n;i++){
+        totStr+=str[i];
+    }
+    
+    for(int i=0;i<totStr.length();i++){
+        System.out.print(totStr.charAt(i));
+        if((i+1)%5==0)
+        System.out.println();
+        
+    }
+    }
+}
