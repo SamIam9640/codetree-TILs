99ed280a0280881466ffe57ba3047883504c51e2
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-06-17 03:11:53 +0900

    Title: 특정 문자의 유무, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240617/특정 문자의 유무/specific-character-presence.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+    String str=sc.next();
+    boolean exists1=false;
+
+    for(int i=0;i<str.length()-1;i++){
+        if(str.charAt(i)=='e'&&str.charAt(i+1)=='e'){
+            exists1=true;
+        }
+    }
+    if(exists1==true)
+    System.out.print("Yes ");
+    else
+    System.out.print("No ");
+
+    boolean exists2=false;
+
+    for(int i=0;i<str.length()-1;i++){
+        if(str.charAt(i)=='a'&&str.charAt(i)=='b'){
+            exists2=true;
+        }
+    }
+
+    if(exists2==true)
+    System.out.print("Yes");
+    else
+    System.out.print("No");
+
+    }
+}
