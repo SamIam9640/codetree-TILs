c1aca7acccb4d8916e1a3a687f9978850d172749
SamIam9640 <153511602+SamIam9640@users.noreply.github.com>
2024-07-02 15:54:20 +0900

    Title: 문자 제거하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240702/문자 제거하기/del-char.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.next();
+
+        int len=s.length();
+
+        while(len>1)
+        {
+            int a=sc.nextInt();
+
+            if(a>=len)
+            a=len-1;
+
+            s=s.substring(0,a)+s.substring(a+1);
+            len--;
+ System.out.println(s);
+        }
+    }
+}
